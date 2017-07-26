207eb1702b8c6ea011b5dc06c9d93d75366b6d0c
i-saint <saint.skr@gmail.com>
2017-07-26 20:24:30 +0900

    PyMeshSync

--- Plugin/PyMeshSync/pch.h
+++ Plugin/PyMeshSync/pch.h
@@ -1,21 +1,8 @@
 #pragma once
 
 #ifdef _WIN32
-    #pragma warning(disable:4996)
     #define NOMINMAX
-    #include <winsock2.h>
     #include <windows.h>
-    #include <amp.h>
-    #include <amp_graphics.h>
-    #include <amp_math.h>
-    #include <ppl.h>
-#else 
-    #include <dlfcn.h>
-    #ifdef __APPLE__
-        #include <mach-o/dyld.h>
-    #else
-        #include <link.h>
-    #endif
 #endif
 #include <cstdio>
 #include <cstdlib>
