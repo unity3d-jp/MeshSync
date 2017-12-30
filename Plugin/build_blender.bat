7775467a24a2927e3fc0f4babedf3d151851417a
i-saint <saint.skr@gmail.com>
2017-12-30 18:40:03 +0900

    blender: made python version selectable (to support both Blender 2.7x and 2.8x)

--- Plugin/build_blender.bat
+++ Plugin/build_blender.bat
@@ -1,11 +1,22 @@
 call buildtools.bat
 
+set PYTHON_VERSION=python3.5
+set PYTHON_LIB_DIR=%cd%\External\python35\lib64
+set PYTHON_INCLUDE_DIR=%cd%\External\python35\include
 msbuild PyMeshSync.vcxproj /t:Build /p:Configuration=Master /p:Platform=x64 /m /nologo
 
-mkdir "UnityMeshSync for Blender"
-mkdir "UnityMeshSync for Blender\MeshSync"
-copy _out\x64_Master\python\MeshSync\*.py "UnityMeshSync for Blender\MeshSync"
-copy _out\x64_Master\python\MeshSync\*.pyd "UnityMeshSync for Blender\MeshSync"
-copy MeshSyncClientBlender\* "UnityMeshSync for Blender"
+set PYTHON_VERSION=python3.6
+set PYTHON_LIB_DIR=%cd%\External\python36\lib64
+set PYTHON_INCLUDE_DIR=%cd%\External\python36\include
+msbuild PyMeshSync.vcxproj /t:Build /p:Configuration=Master /p:Platform=x64 /m /nologo
+
+mkdir "UnityMeshSync for Blender\Blender2.7\MeshSync"
+mkdir "UnityMeshSync for Blender\Blender2.8\MeshSync"
+copy _out\x64_Master\python3.5\MeshSync\*.py "UnityMeshSync for Blender\Blender2.7\MeshSync"
+copy _out\x64_Master\python3.5\MeshSync\*.pyd "UnityMeshSync for Blender\Blender2.7\MeshSync"
+copy MeshSyncClientBlender\* "UnityMeshSync for Blender\Blender2.7"
+copy _out\x64_Master\python3.6\MeshSync\*.py "UnityMeshSync for Blender\Blender2.8\MeshSync"
+copy _out\x64_Master\python3.6\MeshSync\*.pyd "UnityMeshSync for Blender\Blender2.8\MeshSync"
+copy MeshSyncClientBlender\* "UnityMeshSync for Blender\Blender2.8"
 del "UnityMeshSync for Blender.zip"
 powershell.exe -nologo -noprofile -command "Compress-Archive -Path 'UnityMeshSync for Blender\*' -DestinationPath 'UnityMeshSync for Blender.zip'"
