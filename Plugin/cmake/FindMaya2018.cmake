7826199c8b3593d2fa0c8a0c28017a88edd4765a
i-saint <seiya@unity3d.com>
2018-04-16 04:12:10 +0900

    build scripts

--- Plugin/cmake/FindMaya2018.cmake
+++ Plugin/cmake/FindMaya2018.cmake
@@ -7,10 +7,10 @@ set(CMAKE_PREFIX_PATH
     "/opt/autodesk/maya2018"
 )
 
-find_path(MAYA2018_INCLUDE_DIR NAMES maya/MGlobal.h)
+find_path(MAYA2018_INCLUDE_DIR maya/MGlobal.h)
 mark_as_advanced(MAYA2018_INCLUDE_DIR)
 foreach(MAYA_LIB OpenMayaAnim OpenMayaFX OpenMayaRender OpenMayaUI OpenMaya Foundation tbb)
-    find_library(MAYA2018_${MAYA_LIB}_LIBRARY NAMES ${MAYA_LIB} PATHS ${LIBRARY_PATHS} PATH_SUFFIXES lib MacOS)
+    find_file(MAYA2018_${MAYA_LIB}_LIBRARY lib${MAYA_LIB}${CMAKE_SHARED_LIBRARY_SUFFIX} PATH_SUFFIXES lib MacOS)
     mark_as_advanced(MAYA2018_${MAYA_LIB}_LIBRARY)
     if(MAYA2018_${MAYA_LIB}_LIBRARY)
         list(APPEND MAYA2018_LIBRARIES ${MAYA2018_${MAYA_LIB}_LIBRARY})
