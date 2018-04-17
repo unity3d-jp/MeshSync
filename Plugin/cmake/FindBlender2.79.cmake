c33491a10c2c89363c0d4745ff02530002999ca7
i-saint <saint.skr@gmail.com>
2018-04-17 23:21:49 +0900

    mac + blender & maya worked!

--- Plugin/cmake/FindBlender2.79.cmake
+++ Plugin/cmake/FindBlender2.79.cmake
@@ -1,13 +1,18 @@
 # * BLENDER2.79_PYTHON_VERSION
 # * BLENDER2.79_PYTHON_INCLUDE_DIR
 
-set(CMAKE_PREFIX_PATH
-    "/opt/rh/rh-python35/root/usr"
-    "/usr/local/include"
-)
-
 set(BLENDER2.79_PYTHON_VERSION 35 CACHE STRING "")
-find_path(BLENDER2.79_PYTHON_INCLUDE_DIR NAMES Python.h PATH_SUFFIXES python3.5m)
+mark_as_advanced(BLENDER2.79_PYTHON_VERSION)
+find_path(BLENDER2.79_PYTHON_INCLUDE_DIR
+    NAMES
+        "Python.h"
+    PATHS
+        "/opt/rh/rh-python35/root/usr/include"
+        "/usr/local/include"
+    PATH_SUFFIXES
+        "python3.5m"
+    NO_DEFAULT_PATH
+)
 mark_as_advanced(BLENDER2.79_PYTHON_INCLUDE_DIR)
 
 include(FindPackageHandleStandardArgs)
--- Plugin/cmake/FindBlender2.8.cmake
+++ Plugin/cmake/FindBlender2.8.cmake
@@ -1,13 +1,18 @@
 # * BLENDER2.8_PYTHON_VERSION
 # * BLENDER2.8_PYTHON_INCLUDE_DIR
 
-set(CMAKE_PREFIX_PATH
-    "/opt/rh/rh-python36/root/usr"
-    "/usr/local/include"
-)
-
 set(BLENDER2.8_PYTHON_VERSION 36 CACHE STRING "")
-find_path(BLENDER2.8_PYTHON_INCLUDE_DIR NAMES Python.h PATH_SUFFIXES python3.6m)
+mark_as_advanced(BLENDER2.8_PYTHON_VERSION)
+find_path(BLENDER2.8_PYTHON_INCLUDE_DIR
+    NAMES
+        "Python.h"
+    PATHS
+        "/opt/rh/rh-python36/root/usr/include"
+        "/usr/local/include"
+    PATH_SUFFIXES
+        "python3.6m"
+    NO_DEFAULT_PATH
+)
 mark_as_advanced(BLENDER2.8_PYTHON_INCLUDE_DIR)
 
 include(FindPackageHandleStandardArgs)
