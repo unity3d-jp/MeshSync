e83ea273801e28c12e0a8d4b52b21df829759506
i-saint <saint.skr@gmail.com>
2018-05-05 19:27:39 +0900

    blender: cleanup python code

--- Plugin/MeshSyncClientBlender/python/unity_mesh_sync.py
+++ Plugin/MeshSyncClientBlender/python/unity_mesh_sync.py
@@ -1,13 +1,11 @@
-from time import time
 import bpy
-import bmesh
 from bpy.app.handlers import persistent
 import MeshSyncClientBlender as ms
 
 bl_info = {
     "name": "Unity Mesh Sync",
     "author": "Unity Technologies",
-    "version": (2018, 4, 20),
+    "version": (2018, 5, 14),
     "blender": (2, 79),
     "description": "Sync Meshes with Unity",
     "location": "View3D > Mesh Sync",
@@ -16,182 +14,10 @@ bl_info = {
     "category": "Import-Export",
 }
 
+
 msb_context = ms.Context()
-msb_last_sent = 0.0
-msb_updated = []
-msb_added = set()
 
-
-def msb_sync_all():
-    msb_sync(bpy.data.objects)
-
-
-def msb_sync_updated():
-    if not bpy.data.objects.is_updated:
-        return
-    msb_sync([obj for obj in bpy.data.objects if obj.is_updated or obj.is_updated_data])
-
-
-def msb_sync(targets):
-    ctx = msb_context
-    if not ctx.prepare():
-        return
-
-    start_time = time()
-    scene = bpy.context.scene
-
-    # materials
-    for mat in bpy.data.materials:
-        ctx.addMaterial(mat)
-
-    for obj in targets:
-        if not (obj.name in bpy.data.objects):
-            ctx.addDeleted(msb_get_path(obj))
-        elif (obj.type == 'MESH' and scene.meshsync_sync_meshes and not msb_is_particle_system(obj)) or\
-             (obj.type == 'CAMERA' and scene.meshsync_sync_cameras) or\
-             (obj.type == 'LAMP' and scene.meshsync_sync_lights) or\
-             (obj.dupli_group != None):
-            msb_add_object(ctx, obj)
-
-    ctx.send()
-    msb_added.clear()
-    end_time = time()
-    msb_last_sent = end_time
-    #print("msb_sync(): done (", end_time-start_time, "sec)")
-
-
-def msb_add_object(ctx, obj):
-    if obj in msb_added:
-        return None
-    msb_construct_tree(ctx, obj.parent)
-
-    ret = None
-    if obj.type == 'MESH':
-        ret = msb_add_mesh(ctx, obj)
-    elif obj.type == 'CAMERA':
-        ret = msb_add_camera(ctx, obj)
-    elif obj.type == 'LAMP':
-        ret = msb_add_light(ctx, obj)
-    else:
-        ret = msb_add_transform(ctx, obj)
-
-    if ret != None:
-        msb_added.add(obj)
-        #ret.index = obj.pass_index
-    return ret
-
-
-def msb_construct_tree(ctx, obj):
-    if obj == None:
-        return
-    if obj.parent != None:
-        msb_construct_tree(ctx, obj.parent)
-    msb_add_object(ctx, obj)
-
-
-def msb_get_path(obj):
-    path = None
-    if obj.parent != None:
-        path = msb_get_path(obj.parent)
-    else:
-        path = ''
-    path += '/'
-    path += obj.name
-    return path
-
-
-def msb_add_mesh(ctx, obj):
-    path = msb_get_path(obj)
-    dst = ctx.addMesh(obj)
-    msb_handle_dupli_group(ctx, path, obj)
-    if obj.hide:
-        dst.visible = False
-    else:
-        scene = bpy.context.scene
-        dst.visible = obj.is_visible(scene)
-        dst.swap_faces = True
-
-        data = None
-        if scene.meshsync_apply_modifiers:
-            data = obj.to_mesh(scene, True, 'PREVIEW')
-        else:
-            data = obj.data
-
-        material_ids = []
-        for mat in data.materials:
-            material_ids.append(msb_get_material_id(mat))
-
-        ctx.extractMeshData(dst, obj)
-    return dst
-
-
-def msb_add_camera(ctx, obj):
-    path = msb_get_path(obj)
-    dst = ctx.addCamera(obj)
-    ctx.extractCameraData(dst, obj)
-    msb_handle_dupli_group(ctx, path, obj)
-    return dst
-
-
-def msb_add_light(ctx, obj):
-    path = msb_get_path(obj)
-    dst = ctx.addLight(obj)
-    ctx.extractLightData(dst, obj)
-    msb_handle_dupli_group(ctx, path, obj)
-    return dst
-
-
-def msb_add_transform(ctx, obj):
-    path = msb_get_path(obj)
-    dst = ctx.addTransform(obj)
-    ctx.extractTransformData(dst, obj)
-    msb_handle_dupli_group(ctx, path, obj)
-    return dst
-
-
-def msb_add_reference_nodes(ctx, base_path, obj):
-    local_path = msb_get_path(obj)
-    path = base_path + local_path
-    dst = ctx.addTransform(obj)
-    ctx.extractTransformData(dst, obj)
-    msb_handle_dupli_group(ctx, path, obj)
-    dst.reference = local_path
-    for c in obj.children:
-        msb_add_reference_nodes(ctx, path, c)
-    return dst
-
-
-def msb_handle_dupli_group(ctx, base_path, obj):
-    group = obj.dupli_group
-    if group != None:
-        o = group.dupli_offset
-        for c in group.objects:
-            cdst = msb_add_reference_nodes(ctx, base_path, c)
-            if c.parent == None:
-                pos = cdst.position
-                pos[0] -= o.x
-                pos[1] -= o.y
-                pos[2] -= o.z
-                cdst.position = pos
-
-
-def msb_is_particle_system(obj):
-    for mod in obj.modifiers:
-        if mod.type == 'PARTICLE_SYSTEM':
-            return True
-    return False
-
-
-def msb_get_material_id(material):
-    i = 0
-    for mat in bpy.data.materials:
-        if material == mat:
-            return i
-        i += 1
-    return 0
-
-
-def msb_update_settings(self = None, context = None):
+def msb_apply_settings(self = None, context = None):
     ctx = msb_context
     scene = bpy.context.scene
     ctx.server_address = scene.meshsync_server_address
@@ -209,28 +35,27 @@ def msb_update_settings(self = None, context = None):
     ctx.sync_animations = scene.meshsync_sync_animations
     ctx.sample_animation = scene.meshsync_sample_animation
     ctx.animation_sps = scene.meshsync_animation_sps
-    #print("msb_update_settings")
     return None
 
 
 def msb_initialize_properties():
-    bpy.types.Scene.meshsync_server_address = bpy.props.StringProperty(default = "127.0.0.1", name = "Server Address", update = msb_update_settings)
-    bpy.types.Scene.meshsync_server_port = bpy.props.IntProperty(default = 8080, name = "Server Port", update = msb_update_settings)
-    bpy.types.Scene.meshsync_scale_factor = bpy.props.FloatProperty(default = 1.0, name = "Scale Factor", update = msb_update_settings)
-    bpy.types.Scene.meshsync_sync_meshes = bpy.props.BoolProperty(default = True, name = "Sync Meshes", update = msb_update_settings)
-    bpy.types.Scene.meshsync_sync_normals = bpy.props.BoolProperty(default = True, name = "Normals", update = msb_update_settings)
-    bpy.types.Scene.meshsync_sync_uvs = bpy.props.BoolProperty(default = True, name = "UVs", update = msb_update_settings)
-    bpy.types.Scene.meshsync_sync_colors = bpy.props.BoolProperty(default = False, name = "Colors", update = msb_update_settings)
-    bpy.types.Scene.meshsync_sync_bones = bpy.props.BoolProperty(default = True, name = "Bones", update = msb_update_settings)
-    bpy.types.Scene.meshsync_sync_poses = bpy.props.BoolProperty(default = True, name = "Poses", update = msb_update_settings)
-    bpy.types.Scene.meshsync_sync_blendshapes = bpy.props.BoolProperty(default = True, name = "Blend Shapes", update = msb_update_settings)
-    bpy.types.Scene.meshsync_apply_modifiers = bpy.props.BoolProperty(default = False, name = "Apply Modifiers", update = msb_update_settings)
-    bpy.types.Scene.meshsync_sync_cameras = bpy.props.BoolProperty(default = True, name = "Sync Cameras", update = msb_update_settings)
-    bpy.types.Scene.meshsync_sync_lights = bpy.props.BoolProperty(default = True, name = "Sync Lights", update = msb_update_settings)
-    bpy.types.Scene.meshsync_sync_animations = bpy.props.BoolProperty(default = False, name = "Sync Animations", update = msb_update_settings)
-    bpy.types.Scene.meshsync_sample_animation = bpy.props.BoolProperty(default = True, name = "Sample Animations", update = msb_update_settings)
-    bpy.types.Scene.meshsync_animation_sps = bpy.props.IntProperty(default = 5, name = "Samples", update = msb_update_settings)
-    bpy.types.Scene.meshsync_auto_sync = bpy.props.BoolProperty(default = False, name = "Auto Sync", update = msb_update_settings)
+    bpy.types.Scene.meshsync_server_address = bpy.props.StringProperty(default = "127.0.0.1", name = "Server Address")
+    bpy.types.Scene.meshsync_server_port = bpy.props.IntProperty(default = 8080, name = "Server Port")
+    bpy.types.Scene.meshsync_scale_factor = bpy.props.FloatProperty(default = 1.0, name = "Scale Factor")
+    bpy.types.Scene.meshsync_sync_meshes = bpy.props.BoolProperty(default = True, name = "Sync Meshes")
+    bpy.types.Scene.meshsync_sync_normals = bpy.props.BoolProperty(default = True, name = "Normals")
+    bpy.types.Scene.meshsync_sync_uvs = bpy.props.BoolProperty(default = True, name = "UVs")
+    bpy.types.Scene.meshsync_sync_colors = bpy.props.BoolProperty(default = False, name = "Colors")
+    bpy.types.Scene.meshsync_sync_bones = bpy.props.BoolProperty(default = True, name = "Bones")
+    bpy.types.Scene.meshsync_sync_poses = bpy.props.BoolProperty(default = True, name = "Poses")
+    bpy.types.Scene.meshsync_sync_blendshapes = bpy.props.BoolProperty(default = True, name = "Blend Shapes")
+    bpy.types.Scene.meshsync_apply_modifiers = bpy.props.BoolProperty(default = False, name = "Apply Modifiers")
+    bpy.types.Scene.meshsync_sync_cameras = bpy.props.BoolProperty(default = True, name = "Sync Cameras")
+    bpy.types.Scene.meshsync_sync_lights = bpy.props.BoolProperty(default = True, name = "Sync Lights")
+    bpy.types.Scene.meshsync_sync_animations = bpy.props.BoolProperty(default = False, name = "Sync Animations")
+    bpy.types.Scene.meshsync_sample_animation = bpy.props.BoolProperty(default = True, name = "Sample Animations")
+    bpy.types.Scene.meshsync_animation_sps = bpy.props.IntProperty(default = 5, name = "Samples")
+    bpy.types.Scene.meshsync_auto_sync = bpy.props.BoolProperty(default = False, name = "Auto Sync")
 
 
 class MeshSyncPanel(bpy.types.Panel):
@@ -274,9 +99,9 @@ class MeshSync_OpSyncAll(bpy.types.Operator):
     bl_idname = "meshsync.sync_all"
     bl_label = "Sync All"
     def execute(self, context):
+        msb_apply_settings()
         msb_context.setup()
         msb_context.syncAll()
-        #msb_sync_all()
         return{'FINISHED'}
 
 
@@ -285,8 +110,8 @@ def on_scene_update(context):
     msb_context.setup()
     msb_context.flushPendingList();
     if(bpy.context.scene.meshsync_auto_sync):
+        msb_apply_settings()
         msb_context.syncUpdated()
-        #msb_sync_updated()
 
 def register():
     msb_initialize_properties()
