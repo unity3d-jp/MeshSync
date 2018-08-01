b8feb395add54e66adf5f583d12d254b66bd8474
i-saint <saint.skr@gmail.com>
2018-08-01 21:21:16 +0900

    test

--- Plugin/Test/TestMeshSyncClient.cpp
+++ Plugin/Test/TestMeshSyncClient.cpp
@@ -4,6 +4,22 @@
 #include "../MeshSync//msClient.h"
 using namespace mu;
 
+static void Send(ms::Scene& scene)
+{
+    for (auto& obj : scene.objects) {
+        if (auto *mesh = dynamic_cast<ms::Mesh*>(obj.get())) {
+            mesh->setupFlags();
+        }
+    }
+
+    ms::ClientSettings settings;
+    ms::Client client(settings);
+
+    ms::SetMessage mes;
+    mes.scene = std::move(scene);
+    client.send(mes);
+}
+
 TestCase(Test_SendMesh)
 {
     ms::Scene scene;
@@ -12,7 +28,10 @@ TestCase(Test_SendMesh)
         auto mesh = ms::Mesh::create();
         scene.objects.push_back(mesh);
 
-        mesh->path = "/pmsMesh";
+        mesh->path = "/Test/Quad";
+        mesh->flags.has_refine_settings = 1;
+        mesh->refine_settings.flags.gen_normals = 1;
+        mesh->refine_settings.flags.gen_tangents = 1;
 
         auto& points = mesh->points;
         auto& uv = mesh->uv0;
@@ -38,13 +57,34 @@ TestCase(Test_SendMesh)
         indices.push_back(3);
     }
 
-    {
-        ms::ClientSettings settings;
-        ms::Client client(settings);
+    Send(scene);
+}
 
-        ms::SetMessage mes;
-        mes.scene = std::move(scene);
-        client.send(mes);
+TestCase(Test_SendMeshAnimated)
+{
+    for (int i = 0; i < 8; ++i) {
+        ms::Scene scene;
+
+        auto mesh = ms::Mesh::create();
+        scene.objects.push_back(mesh);
+
+        mesh->path = "/Test/Wave";
+        mesh->flags.has_refine_settings = 1;
+        mesh->refine_settings.flags.gen_normals = 1;
+        mesh->refine_settings.flags.gen_tangents = 1;
+
+
+        auto& points = mesh->points;
+        auto& uv = mesh->uv0;
+        auto& counts = mesh->counts;
+        auto& indices = mesh->indices;
+        auto& mids = mesh->material_ids;
+
+        GenerateWaveMesh(counts, indices, points, uv, 2.0f, 1.0f, 32, 30.0f * mu::Deg2Rad * i);
+        mids.resize(counts.size(), 0);
+
+        Send(scene);
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 }
 
