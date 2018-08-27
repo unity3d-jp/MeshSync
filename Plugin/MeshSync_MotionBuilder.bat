0603f5b56037150db58864e5100d3ef55f0eda8b
i-saint <saint.skr@gmail.com>
2018-08-27 17:47:52 +0900

    MotionBuilder wip

---
+++ Plugin/MeshSync_MotionBuilder.bat
@@ -0,0 +1,4 @@
+set MOTIONBUILDER_VERSION=2018
+set MOTIONBUILDER_LIB_DIR=%cd%\External\OpenRealitySDK2018\lib
+set MOTIONBUILDER_INCLUDE_DIR=%cd%\External\OpenRealitySDK2018\include
+MeshSync_MotionBuilder.sln
