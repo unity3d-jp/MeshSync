fa77b35c1d18fb1d555ba185863ac56d21cec6ff
i-saint <saint.skr@gmail.com>
2018-11-02 09:51:33 +0900

    blender: build script

--- Plugin/build_blender.bat
+++ Plugin/build_blender.bat
@@ -1,11 +1,12 @@
 call toolchain.bat
-call :Build 2.79 35
-call :Build 2.80 37
+call :Build 2.79 35 "2, 79, 0"
+call :Build 2.80 37 "2, 80, 0"
 exit /B 0
 
 :Build
     set BLENDER_VERSION=%~1
     set BLENDER_PYTHON_VERSION=%~2
+    set BLENDER_MODULE_BLENDER_VERSION=%~3
     set BLENDER_INCLUDE_DIRS=^
 %cd%\External\blender-%BLENDER_VERSION%\include\blenkernel;^
 %cd%\External\blender-%BLENDER_VERSION%\include\blenlib;^
@@ -24,4 +25,5 @@ exit /B 0
     set DIST_DIR="dist\UnityMeshSync_Blender_Windows\blender-%BLENDER_VERSION%"
     xcopy /YS MeshSyncClientBlender\python "%DIST_DIR%\"
     xcopy /Y _out\x64_Master\MeshSyncClientBlender-%BLENDER_VERSION%\*.pyd "%DIST_DIR%\MeshSyncClientBlender\"
+    MeshSyncClientBlender\tools\replace_blender_version.py "%DIST_DIR%\unity_mesh_sync.py" "%BLENDER_MODULE_BLENDER_VERSION%"
     exit /B 0
