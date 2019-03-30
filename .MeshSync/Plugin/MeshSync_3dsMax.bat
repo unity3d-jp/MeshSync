e7bc3ce281999af9ce5c2661971038531d466934
i-saint <saint.skr@gmail.com>
2019-03-30 11:20:54 +0900

    max: 2020

--- .MeshSync/Plugin/MeshSync_3dsMax.bat
+++ .MeshSync/Plugin/MeshSync_3dsMax.bat
@@ -1,4 +1,4 @@
-set MAX_VERSION=2019
+set MAX_VERSION=2020
 set MAX_LIB_DIR=%cd%\External\3ds Max %MAX_VERSION% SDK\maxsdk\lib\x64\Release
 set MAX_INCLUDE_DIR=%cd%\External\3ds Max %MAX_VERSION% SDK\maxsdk\include
 MeshSync_3dsMax.sln
