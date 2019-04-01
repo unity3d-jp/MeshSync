381ddb68817e6fad6babb0769a22a29ee0775e97
i-saint <saint.skr@gmail.com>
2019-04-02 07:40:17 +0900

    modo: improve build script

--- .MeshSync/Plugin/MeshSync_Modo.bat
+++ .MeshSync/Plugin/MeshSync_Modo.bat
@@ -1,4 +1,3 @@
-set MODO_VERSION=12
 set MODO_SDK_DIR=LXSDK_525410
 set MODO_INCLUDE_DIR=%cd%\External\%MODO_SDK_DIR%\include
 set MODO_SOURCE_DIR=%cd%\External\%MODO_SDK_DIR%\common
--- .MeshSync/Plugin/build_modo.bat
+++ .MeshSync/Plugin/build_modo.bat
@@ -1,10 +1,9 @@
 call toolchain.bat
-call :Build 12 LXSDK_525410
+call :Build LXSDK_525410
 exit /B 0
 
 :Build
-    set MODO_VERSION=%~1
-    set MODO_SDK_DIR=%~2
+    set MODO_SDK_DIR=%~1
     set MODO_INCLUDE_DIR=%cd%\External\%MODO_SDK_DIR%\include
     set MODO_SOURCE_DIR=%cd%\External\%MODO_SDK_DIR%\common
 
@@ -15,5 +14,5 @@ exit /B 0
     )
     
     set DIST_DIR="dist\UnityMeshSync_Modo_Windows"
-    xcopy /Y _out\x64_Release\MeshSyncClientModo%MODO_VERSION%\MeshSyncClientModo.lx "%DIST_DIR%\"
+    xcopy /Y _out\x64_Release\MeshSyncClientModo\MeshSyncClientModo.lx "%DIST_DIR%\"
     exit /B 0
