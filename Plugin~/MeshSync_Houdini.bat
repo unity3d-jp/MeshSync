41a9246663a8ed39b73fa28799fc6d2ed4377c25
i-saint <seiya@unity3d.com>
2019-09-24 18:14:07 +0900

    houdini: wip

--- Plugin~/MeshSync_Houdini.bat
+++ Plugin~/MeshSync_Houdini.bat
@@ -1,5 +1,5 @@
 set HOUDINI_VERSION=17.5
-set HOUDINI_INCLUDE_DIR=C:\SSD\Libs\Houdini 17.5.360\toolkit\include
-set HOUDINI_LIBRARY_DIR=C:\SSD\Libs\Houdini 17.5.360\custom\houdini\dsolib
+set HOUDINI_INCLUDE_DIR=%cd%\External\Houdini 17.5.360\toolkit\include
+set HOUDINI_LIBRARY_DIR=%cd%\External\Houdini 17.5.360\custom\houdini\dsolib
 set HOUDINI_PLUGINS_DIR=%USERPROFILE%\Documents\houdini17.5\dso
 MeshSync_Houdini.sln
