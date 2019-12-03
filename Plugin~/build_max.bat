dee440463e348ea4f1ef6193197acfe2a76adea1
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2019-12-03 14:20:58 +0900

    fix: Delay updating records when there are a lot of simultaneous renames (updates) (#109)
    * Delay updating records when there are a lot of simultaneous updates in Max (like, Interactive Universal Renamer for example)
    
    * Upgrading project to Unity 2019.2.13
    
    * Ignore MeshSync~/Logs
    
    * Fix Max build pipeline.
    
    Requires the following to be set on user's PC
    - MAX_INCLUDE_DIR_YEAR
    - MAX_LIB_DIR_YEAR
    
    Also, make sure that we are only using VS 2017 for building

--- Plugin~/build_max.bat
+++ Plugin~/build_max.bat
@@ -1,4 +1,5 @@
 setlocal
+
 cd /d "%~dp0"
 call toolchain.bat
 call :Build 2020
@@ -10,8 +11,12 @@ exit /B 0
 
 :Build
     set MAX_VERSION=%~1
-    set MAX_LIB_DIR=%cd%\External\3ds Max %MAX_VERSION% SDK\maxsdk\lib\x64\Release
-    set MAX_INCLUDE_DIR=%cd%\External\3ds Max %MAX_VERSION% SDK\maxsdk\include
+    set "INCLUDE_VAR=MAX_INCLUDE_DIR_%MAX_VERSION%"
+    set "LIB_VAR=MAX_LIB_DIR_%MAX_VERSION%"
+    
+    call set MAX_INCLUDE_DIR=%%%INCLUDE_VAR%%%
+    call set MAX_LIB_DIR=%%%LIB_VAR%%%
+       
     msbuild MeshSyncClient3dsMax.vcxproj /t:Build /p:Configuration=Release /p:Platform=x64 /m /nologo
     IF %ERRORLEVEL% NEQ 0 (
         pause
