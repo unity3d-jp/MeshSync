ea74b86afa12faca971945119f214bc5738153a5
Sindharta Tanuwijaya <sindharta@gmail.com>
2020-01-23 16:03:36 +0900

    - Use ADSK_3DSMAX_SDK_ in build_max.bat too - Automatically build Morpher.lib if it doesn't exist

--- Plugin~/MeshSync_3dsMax.bat
+++ Plugin~/MeshSync_3dsMax.bat
@@ -7,7 +7,6 @@ call set MAX_LIB_DIR=%%%MAX_SDK_PATH%%%\lib\x64\Release
 call set MAX_SAMPLES_DIR=%%%MAX_SDK_PATH%%%\samples
 call set MAX_MORPHER_LIB_DIR=%%%MAX_SDK_PATH%%%\samples\modifiers\morpher\Lib\x64\Release
 
-echo %MAX_MORPHER_LIB_DIR%\Morpher.lib
 if not exist "%MAX_MORPHER_LIB_DIR%\Morpher.lib" (
     toolchain.bat
     msbuild "%MAX_SAMPLES_DIR%\modifiers\morpher\morpher.vcxproj" /t:Build /p:Configuration=Release /p:Platform=x64 /m /nologo /p:PlatformToolset=v141 /p:WindowsTargetPlatformVersion=%WindowsSDKVersion%
--- Plugin~/build_max.bat
+++ Plugin~/build_max.bat
@@ -11,10 +11,16 @@ exit /B 0
 
 :Build
     set MAX_VERSION=%~1
-    set "MAX_SDK_PATH=MAX_SDK_%MAX_VERSION%"
+    set "MAX_SDK_PATH=ADSK_3DSMAX_SDK_%MAX_VERSION%"
     
     call set MAX_INCLUDE_DIR=%%%MAX_SDK_PATH%%%\include
     call set MAX_LIB_DIR=%%%MAX_SDK_PATH%%%\lib\x64\Release
+    call set MAX_SAMPLES_DIR=%%%MAX_SDK_PATH%%%\samples
+    call set MAX_MORPHER_LIB_DIR=%%%MAX_SDK_PATH%%%\samples\modifiers\morpher\Lib\x64\Release
+
+    if not exist "%MAX_MORPHER_LIB_DIR%\Morpher.lib" (
+        msbuild "%MAX_SAMPLES_DIR%\modifiers\morpher\morpher.vcxproj" /t:Build /p:Configuration=Release /p:Platform=x64 /m /nologo /p:PlatformToolset=v141 /p:WindowsTargetPlatformVersion=%WindowsSDKVersion%
+    )
           
     msbuild MeshSyncClient3dsMax.vcxproj /t:Build /p:Configuration=Release /p:Platform=x64 /m /nologo
     IF %ERRORLEVEL% NEQ 0 (
