8aeab4bdff011250d55978182fc972abcabcb732
Sindharta Tanuwijaya <sindharta@gmail.com>
2020-01-23 17:16:10 +0900

    Fix paths in MeshSyncClientBlender to compile successfully

--- PluginBlender~/MeshSync_Blender.bat
+++ PluginBlender~/MeshSync_Blender.bat
@@ -1,12 +1,13 @@
 set BLENDER_VERSION=2.79
 set BLENDER_PYTHON_VERSION=35
+set PLUGIN_DIR=%cd%\..\Plugin~
 set BLENDER_INCLUDE_DIRS=^
-%cd%\External\blender-%BLENDER_VERSION%\include\blenkernel;^
-%cd%\External\blender-%BLENDER_VERSION%\include\blenlib;^
-%cd%\External\blender-%BLENDER_VERSION%\include\bmesh;^
-%cd%\External\blender-%BLENDER_VERSION%\include\makesdna;^
-%cd%\External\blender-%BLENDER_VERSION%\include\makesrna;^
-%cd%\External\blender-%BLENDER_VERSION%\include\python;
-set BLENDER_PYTHON_INCLUDE_DIR=%cd%\External\python%BLENDER_PYTHON_VERSION%\include
-set BLENDER_PYTHON_LIB_DIR=%cd%\External\python%BLENDER_PYTHON_VERSION%\lib64
+%PLUGIN_DIR%\External\blender-%BLENDER_VERSION%\include\blenkernel;^
+%PLUGIN_DIR%\External\blender-%BLENDER_VERSION%\include\blenlib;^
+%PLUGIN_DIR%\External\blender-%BLENDER_VERSION%\include\bmesh;^
+%PLUGIN_DIR%\External\blender-%BLENDER_VERSION%\include\makesdna;^
+%PLUGIN_DIR%\External\blender-%BLENDER_VERSION%\include\makesrna;^
+%PLUGIN_DIR%\External\blender-%BLENDER_VERSION%\include\python;
+set BLENDER_PYTHON_INCLUDE_DIR=%PLUGIN_DIR%\External\python%BLENDER_PYTHON_VERSION%\include
+set BLENDER_PYTHON_LIB_DIR=%PLUGIN_DIR%\External\python%BLENDER_PYTHON_VERSION%\lib64
 MeshSync_Blender.sln
