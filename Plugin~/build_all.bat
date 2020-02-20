3a3e09919b9f52be9df703ad6936e63303bccfc0
Sindharta Tanuwijaya <>
2020-02-20 11:46:06 +0900

    - Delete DCC Plugin build scripts

--- Plugin~/build_all.bat
+++
@@ -1,8 +0,0 @@
-cd /d "%~dp0"
-start /wait cmd /k "call build_core.bat && exit"
-start /wait cmd /k "call build_mq.bat && exit"
-start /wait cmd /k "call build_blender.bat && exit"
-start /wait cmd /k "call build_maya.bat && exit"
-start /wait cmd /k "call build_max.bat && exit"
-start /wait cmd /k "call build_motionbuilder.bat && exit"
-start /wait cmd /k "call build_modo.bat && exit"
--- Plugin~/cmake/FindMetasequoia4.70.cmake
+++
@@ -1,9 +0,0 @@
-# * MQSDK_DIR
-
-set(EXTERNAL_DIR "${CMAKE_SOURCE_DIR}/External")
-set(MQSDK_ARCHIVE "${EXTERNAL_DIR}/mqsdk470.zip")
-set(MQSDK_DIR "${EXTERNAL_DIR}/mqsdk470/mqsdk")
-if(NOT EXISTS "${MQSDK_ARCHIVE}")
-    file(DOWNLOAD "http://www.metaseq.net/metaseq/mqsdk470.zip" "${MQSDK_ARCHIVE}" SHOW_PROGRESS)
-    execute_process(WORKING_DIRECTORY "${EXTERNAL_DIR}" COMMAND 7za x -aos -o"mqsdk470" "${MQSDK_ARCHIVE}")
-endif()
