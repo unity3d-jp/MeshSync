9b61afda85e27a971016239d2fc913a2bc22856f
Sindharta Tanuwijaya <sindharta@gmail.com>
2020-02-25 14:31:43 +0900

    Simplify FindPoco

--- Plugin~/Build/cmake_modules/FindPoco.cmake
+++ Plugin~/Build/cmake_modules/FindPoco.cmake
@@ -112,19 +112,13 @@ foreach( component ${components} )
         list(APPEND Poco_INCLUDE_DIRS ${Poco_${component}_INCLUDE_DIR})
     endif()
 
-    # Determine the library name
-if (WIN32)            
-    set(Poco_component_lib_name Poco${component}mt.lib)
-else()
-    set(Poco_component_lib_name libPoco${component}.a)
-endif()
-
     # release library
     if(NOT Poco_${component}_LIBRARY)
         find_library(
             Poco_${component}_LIBRARY 
             NAMES             
-                ${Poco_component_lib_name}
+                Poco${component}mt.lib # Windows
+                libPoco${component}.a  # Others
             HINTS 
                 ${Poco_ROOT_DIR}
                 ${Poco_ROOT_DIR}/cmake-build
