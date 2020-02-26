afe37eeeaaff1102487e3a939ed399e83c2a0e36
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-02-26 15:19:43 +0900

    Create initial test

---
+++ Tests/Editor/MeshSyncServerTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace UnityEditor.MeshSync.Tests {
+public class MeshSyncServerTest  {
+    [Test]
+    public void CreateServer()
+    {
+        MeshSyncServerEditor.CreateMeshSyncServer();
+        
+        //[TODO-sin: 2020-2-26] Add test to make sure that the StreamingAssets are populated
+        Assert.True(true);
+        
+    }
+
+}
+
+} //end namespace
