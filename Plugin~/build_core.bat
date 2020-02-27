8a61607650e7692a52be0a18f8a4ccb29c8f6fd1
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-02-27 15:24:41 +0900

    Delete unused build_core.bat

--- Plugin~/build_core.bat
+++
@@ -1,9 +0,0 @@
-setlocal
-cd /d "%~dp0"
-call toolchain.bat
-
-msbuild mscore.vcxproj /t:Build /p:Configuration=Release /p:Platform=x64 /m /nologo
-IF %ERRORLEVEL% NEQ 0 (
-    pause
-    exit /B 1
-)
