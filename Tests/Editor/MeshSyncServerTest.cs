f57ea5141f07a71b1f8dca9d1fe7d4458ca9af5f
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-02-27 15:50:17 +0900

    Reenable test

--- Tests/Editor/MeshSyncServerTest.cs
+++ Tests/Editor/MeshSyncServerTest.cs
@@ -5,7 +5,7 @@ internal class MeshSyncServerTest  {
     [Test]
     public void CreateServer()
     {
-//        MeshSyncServerEditor.CreateMeshSyncServer();
+        MeshSyncServerEditor.CreateMeshSyncServer();
         
         //[TODO-sin: 2020-2-26] Add test to make sure that the StreamingAssets are populated
         Assert.True(true);
