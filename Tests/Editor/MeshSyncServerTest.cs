18c90312c1b426f2a385b63087b295c87180b2b4
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-03-05 22:36:25 +0900

    Add test to test auto/manual server

--- Tests/Editor/MeshSyncServerTest.cs
+++ Tests/Editor/MeshSyncServerTest.cs
@@ -1,16 +1,39 @@
-﻿using NUnit.Framework;
+﻿using System.Collections;
+using NUnit.Framework;
+using Unity.MeshSync;
+using UnityEditor.SceneManagement;
+using UnityEngine.TestTools;
 
 namespace UnityEditor.MeshSync.Tests {
 internal class MeshSyncServerTest  {
-    [Test]
-    public void CreateServer()
-    {
-        MeshSyncServerEditor.CreateMeshSyncServer();
+    [UnityTest]
+    public IEnumerator CreateAutoServer()  {
+        EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects);
+        MeshSyncServer mss = MeshSyncServerEditor.CreateMeshSyncServer(true);
+        Assert.IsTrue(mss.IsServerStarted());
+
+
         
         //[TODO-sin: 2020-2-26] Add test to make sure that the StreamingAssets are populated
         Assert.True(true);
         
+        yield return null;
     }
+    
+//----------------------------------------------------------------------------------------------------------------------    
+    [UnityTest]
+    public IEnumerator CreateManualServer()  {
+        EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects);
+        MeshSyncServer mss = MeshSyncServerEditor.CreateMeshSyncServer(false);
+        Assert.IsFalse(mss.IsServerStarted());
+
+        yield return null;
+        Assert.IsFalse(mss.IsServerStarted()); //because the default is auto
+
+        mss.StartServer();
+        yield return null;
+        Assert.IsTrue(mss.IsServerStarted());
+    }    
 
 }
 
