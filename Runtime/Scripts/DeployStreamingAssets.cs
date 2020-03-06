47fbc95ddb43ee04d2e37bc07b1c87ad4ffe2a02
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-03-06 15:09:42 +0900

    Copy the server assets to StreamingAssets when the server is started

--- Runtime/Scripts/DeployStreamingAssets.cs
+++ Runtime/Scripts/DeployStreamingAssets.cs
@@ -1,66 +1,71 @@
 #if UNITY_EDITOR
-using System;
 using System.IO;
 using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEditor;
 
-namespace Unity.MeshSync
-{
-    public static class DeployStreamingAssets
-    {
-        const string AssetDirName = "MeshSyncServerRoot";
+namespace Unity.MeshSync {
+    internal static class DeployStreamingAssets  {
 
-        public static bool Deploy(bool overwrite = false)
-        {
-            try
-            {
-                string thisFile = new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileName().Replace('\\', '/');
-                var match = new Regex(@"^(.+?Assets)/Runtime").Match(thisFile);
-                if (match.Success)
-                {
-                    var srcPath = match.Groups[1].Value + "/StreamingAssets/" + AssetDirName;
-                    var dstPath = Application.streamingAssetsPath + "/" + AssetDirName;
-                    if (!Directory.Exists(dstPath))
-                        return CopyDirectory(srcPath, dstPath, overwrite);
-                    return true;
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.LogWarning(e);
-            }
-            return false;
+        internal static bool Deploy(bool overwrite = false) {
+            const string SERVER_ROOT_DIR_NAME = "MeshSyncServerRoot";
+            string serverRootSrcDir = Path.Combine("Packages","com.unity.meshsync","Editor",SERVER_ROOT_DIR_NAME);
+
+            string srcPath = Path.GetFullPath(serverRootSrcDir);
+            if (!Directory.Exists(srcPath))
+                return false;
+            
+            string dstPath = Path.Combine(Application.streamingAssetsPath,SERVER_ROOT_DIR_NAME);
+            return CopyDirectory(srcPath, dstPath, overwrite);
         }
+        
+//----------------------------------------------------------------------------------------------------------------------                
 
         static bool CopyDirectory(string src, string dst, bool overwrite = false)
         {
-            var dir = new DirectoryInfo(src);
+            DirectoryInfo dir = new DirectoryInfo(src);
             if (!dir.Exists)
                 return false;
-            if (!Directory.Exists(dst))
-                Directory.CreateDirectory(dst);
+            
+            Directory.CreateDirectory(dst);
 
-            var isMeta = new Regex(@"\.meta$");
-            foreach (var file in dir.GetFiles())
-            {
+            Regex isMeta = new Regex(@"\.meta$");
+            foreach (FileInfo file in dir.GetFiles()) {
                 if (isMeta.Match(file.Name).Success)
                     continue; // ignore .meta
-
-                var path = Path.Combine(dst, file.Name);
-                file.CopyTo(path, overwrite);
+                
+                string destPath = Path.Combine(dst, file.Name);
+                
+                //Skip existing files if overwrite is false
+                if (!overwrite && File.Exists(destPath))
+                    continue;
+                
+                file.CopyTo(destPath, overwrite);
 
                 // ImportAsset() require relative path from Assets/
-                var importPath = new Regex(@".+?/Assets/").Replace(path, "Assets/");
+                string importPath = NormalizeAssetPath(destPath);
                 AssetDatabase.ImportAsset(importPath);
             }
 
             // recurse
-            foreach (var subdir in dir.GetDirectories())
-                CopyDirectory(subdir.FullName, Path.Combine(dst, subdir.Name), overwrite);
+            foreach (DirectoryInfo subDir in dir.GetDirectories())
+                CopyDirectory(subDir.FullName, Path.Combine(dst, subDir.Name), overwrite);
 
             return true;
         }
+        
+//----------------------------------------------------------------------------------------------------------------------
+
+        //Normalize so that the path is relative to the Unity root project
+        static string NormalizeAssetPath(string path) {
+            if (string.IsNullOrEmpty(path))
+                return null;
+
+            if (path.StartsWith(Application.dataPath)) {
+                return path.Substring(Application.dataPath.Length - "Assets".Length);
+            }
+            return path;
+        }        
 
     }
 }
