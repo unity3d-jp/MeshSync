e4184c3b1bf4678f48454c807d5f968c4d9672ba
Sindharta Tanuwijaya <>
2020-04-24 18:40:50 +0900

    Cleanup

--- Editor/ProjectSettings/MeshSyncSettingsProvider.cs
+++ Editor/ProjectSettings/MeshSyncSettingsProvider.cs
@@ -42,8 +42,6 @@ class MyCustomSettings : ScriptableObject
 
 class MeshSyncSettingsProvider : SettingsProvider {
 
-	
-	
 	MeshSyncSettingsProvider() : base(PROJECT_SETTINGS_MENU_PATH,SettingsScope.Project) {
 		m_tabs = new IMeshSyncSettingsTab[MeshSyncEditorConstants.MAX_SETTINGS_TAB];
 		m_tabs[MeshSyncEditorConstants.GENERAL_SETTINGS_TAB] = new GeneralSettingsTab();
@@ -82,38 +80,12 @@ class MeshSyncSettingsProvider : SettingsProvider {
 			m_content = rootElement1.Query<VisualElement>("Content");
 			SetupTab(MeshSyncEditorConstants.DCC_TOOLS_SETTINGS_TAB);
 
-
-			// var settings = MyCustomSettings.GetSerializedSettings();
-			//
-			// // rootElement is a VisualElement. If you add any children to it, the OnGUI function
-			// // isn't called because the SettingsProvider uses the UIElements drawing framework.
-			// //var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/settings_ui.uss");
-			// //rootElement.styleSheets.Add(styleSheet);
-			// var title = new Label() {
-			// 	text = "Custom UI Elements"
-			// };
-			// title.AddToClassList("title");
-			// title.RegisterCallback<MouseDownEvent>(OnMouseDownEvent);
-			//
-			// rootElement.Add(title);
-			//
-			// var properties = new VisualElement() {
-			// 	style = {
-			// 		flexDirection = FlexDirection.Column
-			// 	}
-			// };
-			// properties.AddToClassList("property-list");
-			// rootElement.Add(properties);
-			//
-			// var tf = new TextField() {
-			// 	value = settings.FindProperty("m_SomeString").stringValue
-			// };
-			// tf.AddToClassList("property-value");
-			// properties.Add(tf);
 		};
 
-		// Populate the search keywords to enable smart search filtering and label highlighting:
-		keywords = new HashSet<string>(new[] {"Number", "Some String"});
+		//TODO-sin: 2020-4-24 Fill in more keywords
+		keywords = new HashSet<string>(new[] {
+			"MeshSync",
+		});
 		
 	}
 	
@@ -180,23 +152,5 @@ class MeshSyncSettingsProvider : SettingsProvider {
 	private const string PROJECT_SETTINGS_UIELEMENTS_PATH = "Packages/com.unity.meshsync/Editor/UIElements/ProjectSettings";
 }
 
-//----------------------------------------------------------------------------------------------------------------------
 	
-	// 	class Styles
-	// 	{
-	// 		public static GUIContent number = new GUIContent("My Number");
-	// 		public static GUIContent someString = new GUIContent("Some string");
-	// 	}
-	// 	
-	// 	// Register the SettingsProvider
-	// 	[SettingsProvider]
-	// 	public static SettingsProvider CreateAssetGraphSettingsProvider() {
-	// 		var provider = new AssetGraphSettingsProvider("Project/MeshSync", SettingsScope.Project);
-	// 		
-	// 		// Automatically extract all keywords from the Styles.
-	// 		provider.keywords = GetSearchKeywordsFromGUIContentProperties<Styles>();			
-	//
-	// 		return provider;
-	// 	}
-	// }
 }
