4ce21590d3066fd1fd0fe3d67e0cdf1052f11923
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-04-28 00:20:38 +0900

    Add test code to check project settings

---
+++ Tests/Editor/ProjectSettingsTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace UnityEditor.MeshSync.Tests {
+internal class ProjectSettingsTest {
+    
+    
+    [Test]
+    public void CheckInstalledDCCTools() {
+
+        Dictionary<string, DCCToolInfo> dccToolInfos = ProjectSettingsUtility.FindInstalledDCCTools();
+        Assert.GreaterOrEqual(dccToolInfos.Count,0);
+    }    
+    
+//----------------------------------------------------------------------------------------------------------------------    
+    [Test]
+    public void CheckCurrentSettings() {
+        MeshSyncProjectSettings settings = MeshSyncProjectSettings.GetOrCreateSettings();
+        Assert.NotNull(settings);
+    }    
+    
+}
+
+} //end namespace
