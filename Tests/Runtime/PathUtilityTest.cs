b64ce477587a05bbe825279337ac15fe42dab751
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-04-28 11:14:03 +0900

    Add utility function to check path

---
+++ Tests/Runtime/PathUtilityTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Unity.AnimeToolbox.Tests {
+    internal class PathUtilityTest
+    {
+
+        [Test]
+        public void GetDirectoryNames() {
+
+            string dirName = null;
+            dirName = PathUtility.TryGetDirectoryName("/Applications/Unity 2019/Unity.app", 1);
+            Assert.AreEqual(dirName, "/Applications/Unity 2019");
+            dirName = PathUtility.TryGetDirectoryName("/Applications/Unity 2019/Unity.app/Contents/MacOS/Unity", 4);
+            Assert.AreEqual(dirName, "/Applications/Unity 2019");
+            
+            //Null checks
+            dirName = PathUtility.TryGetDirectoryName("/Applications/Unity 2019/Unity.app", 4);
+            Assert.IsNull(dirName);
+            dirName = PathUtility.TryGetDirectoryName(null);
+            Assert.IsNull(dirName);
+            
+        }
+
+    }
+    
+}
