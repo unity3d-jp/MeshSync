e5ef637bf024d25d3f2f840afd709b46afb95468
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-04-28 11:29:41 +0900

    Add a test to find DCC Tool versions on OSX

--- Tests/Editor/ProjectSettingsTest.cs
+++ Tests/Editor/ProjectSettingsTest.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace UnityEditor.MeshSync.Tests {
 internal class ProjectSettingsTest {
@@ -17,6 +19,22 @@ internal class ProjectSettingsTest {
     public void CheckCurrentSettings() {
         MeshSyncProjectSettings settings = MeshSyncProjectSettings.GetOrCreateSettings();
         Assert.NotNull(settings);
+    }    
+
+//----------------------------------------------------------------------------------------------------------------------    
+    
+    [Test]
+    [UnityPlatform(RuntimePlatform.OSXEditor)]
+    public void FindDCCToolVersionsOnOSX() {
+
+        string version = null;
+        
+        version = ProjectSettingsUtility.FindMayaVersion("/Applications/Maya 2019/Maya.app/Contents/MacOS/Maya");
+        Assert.AreEqual("2019", version);
+
+        version = ProjectSettingsUtility.FindMayaVersion("/Applications/Maya2019/Maya.app/Contents/MacOS/Maya");
+        Assert.AreEqual("2019", version);
+
     }    
     
 }
