5a5c6778247811771ff7bcd87da711eb1c409744
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-05-07 19:51:56 +0900

    Use PluginDownloader in DCCIntegrationUtility

--- Editor/Scripts/Utilities/DCCIntegrationUtility.cs
+++ Editor/Scripts/Utilities/DCCIntegrationUtility.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.IO;
+using UnityEngine;
 
 
 namespace UnityEditor.MeshSync {
     
 internal static class DCCIntegrationUtility {
 
-    internal static bool InstallPlugin(DCCToolInfo dccToolInfo) {
+    internal static void InstallPlugin(DCCToolInfo dccToolInfo) {
 
         switch (dccToolInfo.Type) {
             case DCCToolType.AUTODESK_MAYA: {
-                return InstallMayaPlugin();
+                InstallMayaPlugin();
+                break;
             }
             case DCCToolType.AUTODESK_3DSMAX: {
                 throw new NotImplementedException();
@@ -22,28 +25,59 @@ internal static class DCCIntegrationUtility {
     
 //----------------------------------------------------------------------------------------------------------------------
 
-    static bool InstallMayaPlugin() {
+    static void InstallMayaPlugin() {
         
-        //[TODO-sin: 2020-5-7] Implement this
-        //Check file if it exists. if it doesn't, try to download it
+        const string PLUGIN_NAME = "Maya";
+        string dccPlatformName = PLUGIN_NAME + "_" + GetCurrentDCCPluginPlatform() + ".zip";
+        
+        //Make sure the file exists first
+        DCCPluginDownloader downloader = new DCCPluginDownloader(false,SAVED_PLUGINS_FOLDER, 
+            new string[] { dccPlatformName }
+        );
+        
+        EditorUtility.DisplayProgressBar("MeshSync", "Installing plugin for " + PLUGIN_NAME,0);
+        downloader.Execute(() => {
+            Debug.Log("File copied to: " + SAVED_PLUGINS_FOLDER);
             
-        //Copy the file to 
-        // Windows:
-        // If MAYA_APP_DIR environment variable is setup, copy the modules directory there.
-        //     If not, go to %USERPROFILE%\Documents\maya in Windows Explorer, and copy the modules directory there.
-        //     Mac:
-        // Copy the UnityMeshSync directory and UnityMeshSync.mod file to /Users/Shared/Autodesk/modules/maya.
-        //     Linux:
-        // Copy the modules directory to ~/maya/<maya_version)            
-        //Set configuration
-                
-        throw new NotImplementedException();
+            //[TODO-sin: 2020-5-7] Implement this
+            //Copy the file to 
+            // Windows:
+            // If MAYA_APP_DIR environment variable is setup, copy the modules directory there.
+            //     If not, go to %USERPROFILE%\Documents\maya in Windows Explorer, and copy the modules directory there.
+            //     Mac:
+            // Copy the UnityMeshSync directory and UnityMeshSync.mod file to /Users/Shared/Autodesk/modules/maya.
+            //     Linux:
+            // Copy the modules directory to ~/maya/<maya_version)            
+            //Set configuration
+            
+            EditorUtility.ClearProgressBar();
+        }, () => {
+            Debug.LogError("Failed to download DCC Plugin for " + PLUGIN_NAME);
+            EditorUtility.ClearProgressBar();
+        });
+    }
+
+//----------------------------------------------------------------------------------------------------------------------    
+
+    static string GetCurrentDCCPluginPlatform() {
+        string platform = null;
+        switch (Application.platform) {
+            case RuntimePlatform.WindowsEditor: platform = "Windows"; break;
+            case RuntimePlatform.OSXEditor:  platform = "Mac"; break;
+            case RuntimePlatform.LinuxEditor:  platform = "Linux"; break;
+            default: {
+                throw new NotImplementedException();
+            }
+        }
+
+        return platform;
 
     }
 
-    
-        
-    
+//----------------------------------------------------------------------------------------------------------------------    
+
+    private static readonly string SAVED_PLUGINS_FOLDER = Path.Combine(Application.dataPath, "MeshSyncDCCPlugins~");
+
 }
 
 } //end namespace
