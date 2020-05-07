33e5d06ea4043be1178bbfb43c7ec619bc8590b2
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-05-07 22:11:22 +0900

    Add test for downloading plugin

--- Tests/Editor/ProjectSettingsTest.cs
+++ Tests/Editor/ProjectSettingsTest.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityScript.Macros;
 
 namespace UnityEditor.MeshSync.Tests {
 internal class ProjectSettingsTest {
@@ -23,19 +27,59 @@ internal class ProjectSettingsTest {
 
 //----------------------------------------------------------------------------------------------------------------------    
     
+    [UnityTest]
+    public IEnumerator DownloadDCCPlugin()  {
+        string destFolder = FileUtil.GetUniqueTempPathInProject();
+
+
+        string[] requestedDCCPlatformNames = new string[] {
+            "Maya_Windows.zip",
+        };
+
+        DCCPluginDownloader downloader = new DCCPluginDownloader(false, destFolder, requestedDCCPlatformNames);
+
+        bool finished = false;
+        downloader.Execute((List<string> dccPluginLocalPaths) => {
+        
+            Assert.AreEqual(requestedDCCPlatformNames.Length, dccPluginLocalPaths.Count);
+
+            //Clean up files
+            foreach (string tempDCCPluginLocalPath in dccPluginLocalPaths) {
+                Assert.True(File.Exists(tempDCCPluginLocalPath));
+                File.Delete(tempDCCPluginLocalPath);
+            }
+
+            finished = true;
+        }, () =>
+        {
+            finished = true;
+            Assert.Fail();
+            
+        });
+
+        while (!finished) {
+            Thread.Sleep(1000);
+            Debug.Log("Downloading ... ");
+            yield return null;
+        }
+        
+    }    
+//----------------------------------------------------------------------------------------------------------------------    
+    
     [Test]
     [UnityPlatform(RuntimePlatform.OSXEditor)]
     public void FindDCCToolVersionsOnOSX() {
 
+        //[TODO-sin: 2020-5-7] Add tests for Windows and Linux
         string version = null;
-        
         version = DCCFinderUtility.FindMayaVersion("/Applications/Maya 2019/Maya.app/Contents/MacOS/Maya");
         Assert.AreEqual("2019", version);
 
         version = DCCFinderUtility.FindMayaVersion("/Applications/Maya2019/Maya.app/Contents/MacOS/Maya");
         Assert.AreEqual("2019", version);
+        
+    }
 
-    }    
     
 }
 
