c7863067ea0fcacffedf49a2582d5bc1bc37d54e
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-05-08 16:45:21 +0900

    Add DCCPluginInstallInfo class

---
+++ Editor/Scripts/DCCIntegration/DCCPluginInstallInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityEditor.MeshSync {
+
+[Serializable]
+internal class DCCPluginInstallInfo {
+
+    internal DCCPluginInstallInfo(string pluginVersion) {
+        PluginVersion = pluginVersion;
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    /// <summary>
+    /// MeshSync plugin version installed for this DCC tool
+    /// </summary>
+    public string         PluginVersion;
+
+    // ReSharper disable once NotAccessedField.Local
+    [SerializeField] private int m_classVersion = 1;
+
+}
+
+} //end namespace
