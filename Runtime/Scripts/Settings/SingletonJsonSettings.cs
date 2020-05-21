4a618911a07dbc47520d47f697472594b9fefb79
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-05-21 16:57:08 +0900

    Move PathAttribute to a different file

--- Runtime/Scripts/Settings/SingletonJsonSettings.cs
+++ Runtime/Scripts/Settings/SingletonJsonSettings.cs
@@ -91,22 +91,5 @@ internal class SingletonJsonSettings<T> where T: class , new() {
     private static readonly object m_settingsLock = new object();
 }
 
-[AttributeUsage(AttributeTargets.Class)]
-internal class PathAttribute : Attribute {
-      
-    public PathAttribute(string path) {
-        if (string.IsNullOrEmpty(path)) {
-            Debug.LogError("[MeshSync] path is null or empty.");
-            return;
-        }
-
-        m_path = path;
-    }
-
-    internal string GetPath() { return m_path; }
-
-    private readonly string m_path;
-}
-
 
 } //end namespace
