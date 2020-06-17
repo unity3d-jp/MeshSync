035a3965b6c0f4f47b9153a54cf723c69dd1f32f
sindharta <sindharta@gmail.com>
2020-06-17 18:33:06 +0900

    Update version to 0.2.0-preview

--- Plugin~/Src/MeshSync/msConfig.h
+++ Plugin~/Src/MeshSync/msConfig.h
@@ -1,8 +1,8 @@
 #pragma once
 
 //Note: Every update to the plugin must increase the version number
-#define msPluginVersion 20200407
-#define msPluginVersionStr "0.1.0-preview"
+#define msPluginVersion 20200617
+#define msPluginVersionStr "0.2.0-preview"
 #define msVendor "Unity Technologies"
 #define msProtocolVersion 122
 
