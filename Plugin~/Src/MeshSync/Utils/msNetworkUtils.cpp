df1e1fd93af08143e9a72b52565a1594678a44ae
sindharta <sindharta@gmail.com>
2020-07-06 17:33:36 +0900

    Do localhost checking more strictly

--- Plugin~/Src/MeshSync/Utils/msNetworkUtils.cpp
+++ Plugin~/Src/MeshSync/Utils/msNetworkUtils.cpp
@@ -6,7 +6,17 @@
 namespace ms {
 
 bool NetworkUtils::IsLocalHost(const std::string& hostAndPort) {
-    return (StartsWith(hostAndPort,"localhost") || StartsWith(hostAndPort,"127.0.0.1"));
+    const uint32_t MAX_TOKENS = 3;
+    std::vector<std::string> tokens(MAX_TOKENS);
+    std::istringstream input(hostAndPort);
+
+    uint32_t i = 0;
+    while (i < MAX_TOKENS && getline(input, tokens[i], ':')) {
+        ++i;
+    }
+
+
+    return (i < MAX_TOKENS && (tokens[0] == "localhost" || tokens[0] == "127.0.0.1"));
 }
 
 } // namespace ms
