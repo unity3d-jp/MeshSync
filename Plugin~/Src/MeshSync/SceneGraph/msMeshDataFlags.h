988c1dacbf9d79b8d0dd2494703bdc5baae6d069
sindharta <sindharta@gmail.com>
2020-08-28 15:35:46 +0900

    Separate MeshDataFlags to a different file

---
+++ Plugin~/Src/MeshSync/SceneGraph/msMeshDataFlags.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "msIdentifier.h"
+
+namespace ms {
+// must be synced with C# side
+struct MeshDataFlags
+{
+    uint32_t unchanged : 1;         // 0
+    uint32_t topology_unchanged : 1;
+    uint32_t has_refine_settings : 1;
+    uint32_t has_indices : 1;
+    uint32_t has_counts : 1;
+    uint32_t has_points : 1;        // 5
+    uint32_t has_normals : 1;
+    uint32_t has_tangents : 1;
+    uint32_t has_uv0 : 1;
+    uint32_t has_uv1 : 1;
+    uint32_t has_colors : 1;        // 10
+    uint32_t has_velocities : 1;
+    uint32_t has_material_ids : 1;
+    uint32_t has_face_groups : 1; // use upper 16 bit of material id as face group. mainly for 3ds max
+    uint32_t has_root_bone : 1;
+    uint32_t has_bones : 1;         // 15
+    uint32_t has_blendshapes : 1;
+    uint32_t has_blendshape_weights : 1;
+    uint32_t has_submeshes : 1;
+    uint32_t has_bounds: 1;
+
+    uint32_t Unused_20: 1;
+    uint32_t Unused_21: 1;
+    uint32_t Unused_22: 1;
+    uint32_t Unused_23: 1;
+    uint32_t Unused_24: 1;
+    uint32_t Unused_25: 1;
+    uint32_t Unused_26: 1;
+    uint32_t Unused_27: 1;
+    uint32_t Unused_28: 1;
+    uint32_t Unused_29: 1;
+    uint32_t Unused_30: 1;
+    uint32_t Unused_31: 1;
+
+    MeshDataFlags();
+};
+
+
+} // namespace ms
