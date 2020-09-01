238da97d6446508aea04a874da0893a7d0e57ce0
sindharta <sindharta@gmail.com>
2020-09-01 15:39:11 +0900

    Changing PointsDataFlags to bit shifting

--- Plugin~/Src/MeshSync/CoreAPI/msPointsDataFlags.h
+++ Plugin~/Src/MeshSync/CoreAPI/msPointsDataFlags.h
@@ -3,18 +3,65 @@
 namespace ms {
 
 // must be synced with C# side
-struct PointsDataFlags {
-    uint32_t unchanged : 1;         // 0
-    uint32_t topology_unchanged : 1;
-    uint32_t has_points : 1;
-    uint32_t has_rotations : 1;
-    uint32_t has_scales : 1;
-    uint32_t has_colors : 1;        // 5
-    uint32_t has_velocities : 1;
-    uint32_t has_ids : 1;
-    uint32_t has_bounds : 1;
-
-    PointsDataFlags();
+enum PointsDataFlagsBit {
+    POINTS_DATA_FLAG_UNCHANGED = 0,
+    POINTS_DATA_FLAG_TOPOLOGY_UNCHANGED,
+    POINTS_DATA_FLAG_HAS_POINTS,
+    POINTS_DATA_FLAG_HAS_ROTATIONS,
+    POINTS_DATA_FLAG_HAS_SCALES,
+    POINTS_DATA_FLAG_HAS_COLORS,
+    POINTS_DATA_FLAG_HAS_VELOCITIES,
+    POINTS_DATA_FLAG_HAS_IDS,
+    POINTS_DATA_FLAG_HAS_BOUNDS, //8
+    POINTS_DATA_FLAG_UNUSED_9,
+    POINTS_DATA_FLAG_UNUSED_10,
+    POINTS_DATA_FLAG_UNUSED_11,
+    POINTS_DATA_FLAG_UNUSED_12,
+    POINTS_DATA_FLAG_UNUSED_13,
+    POINTS_DATA_FLAG_UNUSED_14,
+    POINTS_DATA_FLAG_UNUSED_15,
+    POINTS_DATA_FLAG_UNUSED_16,
+    POINTS_DATA_FLAG_UNUSED_17,
+    POINTS_DATA_FLAG_UNUSED_18,
+    POINTS_DATA_FLAG_UNUSED_19,
+    POINTS_DATA_FLAG_UNUSED_20,
+    POINTS_DATA_FLAG_UNUSED_21,
+    POINTS_DATA_FLAG_UNUSED_22,
+    POINTS_DATA_FLAG_UNUSED_23,
+    POINTS_DATA_FLAG_UNUSED_24,
+    POINTS_DATA_FLAG_UNUSED_25,
+    POINTS_DATA_FLAG_UNUSED_26,
+    POINTS_DATA_FLAG_UNUSED_27,
+    POINTS_DATA_FLAG_UNUSED_28,
+    POINTS_DATA_FLAG_UNUSED_29,
+    POINTS_DATA_FLAG_UNUSED_30,
+    POINTS_DATA_FLAG_UNUSED_31,
 };
+
+//----------------------------------------------------------------------------------------------------------------------
+
+struct PointsDataFlags {
+
+    PointsDataFlags() : m_bitFlags(0) {}
+    inline void Set(uint32_t index, const bool val);
+    inline bool Get(uint32_t index) const;
+private:
+    uint32_t m_bitFlags = 0;
+};
+
+void PointsDataFlags::Set(uint32_t index, const bool val) {
+    assert(index < (sizeof(index) * 8) && "PointsDataFlags::Get() invalid index");
+    const uint32_t mask = (1 << index);
+    if (val) {
+        m_bitFlags |= mask;
+    } else {
+        m_bitFlags &= ~mask;    
+    }
+}
+
+bool PointsDataFlags::Get(uint32_t index) const {
+    assert(index < (sizeof(index) * 8) && "PointsDataFlags::Get() invalid index");
+    return (m_bitFlags & (1 << index));
+}
 
 } // namespace ms
--- Plugin~/Src/MeshSync/SceneGraph/msPointCache.cpp
+++ Plugin~/Src/MeshSync/SceneGraph/msPointCache.cpp
@@ -5,17 +5,21 @@ namespace ms {
 
 static_assert(sizeof(PointsDataFlags) == sizeof(uint32_t), "");
 
-PointsDataFlags::PointsDataFlags()
-{
-    (uint32_t&)*this = 0;
-}
-
 #pragma region Points
 #define EachArray(F)\
     F(points) F(rotations) F(scales) F(colors) F(velocities) F(ids)
 #define EachMember(F)\
     EachArray(F) F(bounds)
 
+#define SERIALIZE_POINTS(flags, op, stream) {   \
+    if (flags.Get(POINTS_DATA_FLAG_HAS_POINTS))  { op(stream, points); } \
+    if (flags.Get(POINTS_DATA_FLAG_HAS_ROTATIONS))  { op(stream, rotations); } \
+    if (flags.Get(POINTS_DATA_FLAG_HAS_SCALES))  { op(stream, scales); } \
+    if (flags.Get(POINTS_DATA_FLAG_HAS_COLORS))  { op(stream, colors); } \
+    if (flags.Get(POINTS_DATA_FLAG_HAS_VELOCITIES))  { op(stream, velocities); } \
+    if (flags.Get(POINTS_DATA_FLAG_HAS_IDS))  { op(stream, ids); } \
+    if (flags.Get(POINTS_DATA_FLAG_HAS_BOUNDS))  { op(stream, bounds); } \
+}
 
 Points::Points() { clear(); }
 Points::~Points() {}
@@ -26,24 +30,20 @@ void Points::serialize(std::ostream& os) const
 {
     super::serialize(os);
     write(os, pd_flags);
-    if (pd_flags.unchanged)
+    if (pd_flags.Get(POINTS_DATA_FLAG_UNCHANGED))
         return;
 
-#define Body(V) if(pd_flags.has_##V) write(os, V);
-    EachMember(Body);
-#undef Body
+    SERIALIZE_POINTS(pd_flags, write, os);
 }
 
 void Points::deserialize(std::istream& is)
 {
     super::deserialize(is);
     read(is, pd_flags);
-    if (pd_flags.unchanged)
+    if (pd_flags.Get(POINTS_DATA_FLAG_UNCHANGED))
         return;
 
-#define Body(V) if(pd_flags.has_##V) read(is, V);
-        EachMember(Body);
-#undef Body
+    SERIALIZE_POINTS(pd_flags, read, is);
 }
 
 void Points::detach()
@@ -56,23 +56,18 @@ void Points::detach()
 void Points::setupDataFlags()
 {
     super::setupDataFlags();
-    pd_flags.has_points = !points.empty();
-    pd_flags.has_rotations = !rotations.empty();
-    pd_flags.has_scales = !scales.empty();
-    pd_flags.has_colors = !colors.empty();
-    pd_flags.has_velocities = !velocities.empty();
-    pd_flags.has_ids = !ids.empty();
-    pd_flags.has_bounds = bounds != Bounds{};
+    setupPointsDataFlags();
+    pd_flags.Set(POINTS_DATA_FLAG_HAS_BOUNDS, bounds != Bounds{});
 }
 
 bool Points::isUnchanged() const
 {
-    return td_flags.Get(TRANSFORM_DATA_FLAG_UNCHANGED) && pd_flags.unchanged;
+    return td_flags.Get(TRANSFORM_DATA_FLAG_UNCHANGED) && pd_flags.Get(POINTS_DATA_FLAG_UNCHANGED);
 }
 
 bool Points::isTopologyUnchanged() const
 {
-    return pd_flags.topology_unchanged;
+    return pd_flags.Get(POINTS_DATA_FLAG_TOPOLOGY_UNCHANGED);
 }
 
 bool Points::strip(const Entity& base_)
@@ -88,16 +83,16 @@ bool Points::strip(const Entity& base_)
             unchanged = false;
     };
 
-    auto& base = static_cast<const Points&>(base_);
+    const Points& base = dynamic_cast<const Points&>(base_);
     clear_if_identical(ids, base.ids);
-    pd_flags.topology_unchanged = unchanged && points.size() == base.points.size();
+    pd_flags.Set(POINTS_DATA_FLAG_TOPOLOGY_UNCHANGED, unchanged && points.size() == base.points.size());
 
     clear_if_identical(points, base.points);
     clear_if_identical(rotations, base.rotations);
     clear_if_identical(scales, base.scales);
     clear_if_identical(colors, base.colors);
     clear_if_identical(velocities, base.velocities);
-    pd_flags.unchanged = unchanged;
+    pd_flags.Set(POINTS_DATA_FLAG_UNCHANGED, unchanged);
     return true;
 }
 
@@ -107,7 +102,7 @@ bool Points::merge(const Entity& base_)
         return false;
     auto& base = static_cast<const Points&>(base_);
 
-    if (pd_flags.unchanged) {
+    if (pd_flags.Get(POINTS_DATA_FLAG_UNCHANGED)) {
 #define Body(A) A = base.A;
         EachArray(Body);
 #undef Body
@@ -140,14 +135,14 @@ bool Points::diff(const Entity&e1_, const Entity& e2_)
 
 
     compare_attribute(e1.ids, e2.ids);
-    pd_flags.topology_unchanged = unchanged && e1.points.size() == e2.points.size();
+    pd_flags.Set(POINTS_DATA_FLAG_TOPOLOGY_UNCHANGED, unchanged && e1.points.size() == e2.points.size());
 
     compare_attribute(e1.points, e2.points);
     compare_attribute(e1.rotations, e2.rotations);
     compare_attribute(e1.scales, e2.scales);
     compare_attribute(e1.colors, e2.colors);
     compare_attribute(e1.velocities, e2.velocities);
-    pd_flags.unchanged = unchanged;
+    pd_flags.Set(POINTS_DATA_FLAG_UNCHANGED, unchanged);
     return true;
 }
 
@@ -225,12 +220,12 @@ EntityPtr Points::clone(bool detach_)
 
 void Points::setupPointsDataFlags()
 {
-    pd_flags.has_points = !points.empty();
-    pd_flags.has_rotations = !rotations.empty();
-    pd_flags.has_scales = !scales.empty();
-    pd_flags.has_colors = !colors.empty();
-    pd_flags.has_velocities = !velocities.empty();
-    pd_flags.has_ids = !ids.empty();
+    pd_flags.Set(POINTS_DATA_FLAG_HAS_POINTS, !points.empty());
+    pd_flags.Set(POINTS_DATA_FLAG_HAS_ROTATIONS,  !rotations.empty());
+    pd_flags.Set(POINTS_DATA_FLAG_HAS_SCALES,  !scales.empty());
+    pd_flags.Set(POINTS_DATA_FLAG_HAS_COLORS, !colors.empty());
+    pd_flags.Set(POINTS_DATA_FLAG_HAS_VELOCITIES,  !velocities.empty());
+    pd_flags.Set(POINTS_DATA_FLAG_HAS_IDS, !ids.empty());
 }
 
 
