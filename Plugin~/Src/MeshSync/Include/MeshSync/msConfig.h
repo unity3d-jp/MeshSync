69ce2365f4fe54c96943ce8320f3432dd5488593
sindharta <sindharta@gmail.com>
2020-09-07 11:12:51 +0900

    update msConfig.h

--- Plugin~/Src/MeshSync/Include/MeshSync/msConfig.h
+++ Plugin~/Src/MeshSync/Include/MeshSync/msConfig.h
@@ -1,8 +1,8 @@
 #pragma once
 
 //Note: Every update to the plugin must increase the version number
-#define msPluginVersion 20200904
-#define msPluginVersionStr "0.3.1-preview"
+#define msPluginVersion 20200907
+#define msPluginVersionStr "0.3.2-preview"
 #define msVendor "Unity Technologies"
 #define msProtocolVersion 123
 
