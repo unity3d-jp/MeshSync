fcbc90de32b77bf0c5c99f7b8fb28a9db1909a08
sindharta <sindharta@gmail.com>
2020-09-07 10:34:00 +0900

    Declare essential classes/structs in MeshSync.h

--- Plugin~/Src/MeshSync/Include/MeshSync/MeshSync.h
+++ Plugin~/Src/MeshSync/Include/MeshSync/MeshSync.h
@@ -16,10 +16,15 @@ namespace ms { \
 } 
 
 //Entity
+msDeclClassPtr(Camera)
+msDeclClassPtr(Light)
+msDeclClassPtr(Mesh)
 msDeclClassPtr(Transform)
 
 //Assets
+msDeclClassPtr(AnimationClip)
 msDeclClassPtr(Audio)
+msDeclClassPtr(Material)
 msDeclClassPtr(Texture)
 
 //Protocol/Messages
@@ -28,114 +33,9 @@ msDeclClassPtr(TextMessage)
 //Network
 msDeclClassPtr(Server)
 
+//Others
+msDeclClassPtr(TransformAnimation)
 
-//
-//class Entity;
-//msDeclPtr(Entity);
-//
-//class Mesh;
-//msDeclPtr(Mesh);
-//
-//class Points;
-//msDeclPtr(Points);
-//
-//struct BlendShapeFrameData;
-//msDeclPtr(BlendShapeFrameData);
-//
-//struct BlendShapeData;
-//msDeclPtr(BlendShapeData);
-//
-//struct BoneData;
-//msDeclPtr(BoneData);
-//
-//class Points;
-//msDeclPtr(Points);
-//
-//class Mesh;
-//msDeclPtr(Mesh);
-//
-//class Constraint;
-//msDeclPtr(Constraint);
-//
-//class AnimationCurve;
-//msDeclPtr(AnimationCurve);
-//
-//class Scene;
-//msDeclPtr(Scene);
-//
-//class Animation;
-//msDeclPtr(Animation);
-//
-//class TransformAnimation;
-//msDeclPtr(TransformAnimation);
-//
-////Protocol/Messages
-//class ResponseMessage;
-//msDeclPtr(ResponseMessage);
-//
-//class QueryMessage;
-//msDeclPtr(QueryMessage);
-//
-//class PollMessage;
-//msDeclPtr(PollMessage);
-//
-//class SetMessage;
-//msDeclPtr(SetMessage);
-//
-//class DeleteMessage;
-//msDeclPtr(DeleteMessage);
-//
-//class FenceMessage;
-//msDeclPtr(FenceMessage);
-//
-//
+//Structs
+msDeclStructPtr(BlendShapeData)
 
-//
-//class Message;
-//msDeclPtr(Message);
-//
-////[Note-sin: 2020-9-4] Might be defined in Windows by Winsock
-////#ifdef GetMessage 
-////#undef GetMessage
-////#endif
-//
-//class GetMessage;
-//msDeclPtr(GetMessage);
-//
-//class ScreenshotMessage;
-//msDeclPtr(ScreenshotMessage);
-//
-//
-
-
-//
-//class AnimationClip;
-//msDeclPtr(AnimationClip);
-//
-//class FileAsset;
-//msDeclPtr(FileAsset);
-//
-//class Material;
-//msDeclPtr(Material);
-//
-//
-////BlendShape
-//struct BlendShapeFrameData;
-//msDeclPtr(BlendShapeFrameData);
-//
-//
-////Converter / Correctors
-//class EntityConverter;
-//msDeclPtr(EntityConverter);
-//
-//class ScaleConverter;
-//msDeclPtr(ScaleConverter);
-//
-//class FlipX_HandednessCorrector;
-//msDeclPtr(FlipX_HandednessCorrector);
-//
-//class FlipYZ_ZUpCorrector;
-//msDeclPtr(FlipYZ_ZUpCorrector);
-//
-//class RotateX_ZUpCorrector;
-//msDeclPtr(RotateX_ZUpCorrector);
