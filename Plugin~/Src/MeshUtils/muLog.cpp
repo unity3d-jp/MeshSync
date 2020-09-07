9b9cdaaf28f9616d9ee1df8dc9cdbedf0fb517fe
sindharta <sindharta@gmail.com>
2020-09-07 10:24:29 +0900

    Separate muLog from muMisc

---
+++ Plugin~/Src/MeshUtils/muLog.cpp
@@ -0,0 +1,34 @@
+#include "pch.h"
+#include "muLog.h"
+
+namespace mu { 
+
+void Print(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+#ifdef _WIN32
+    char buf[1024];
+    vsnprintf(buf, sizeof(buf), fmt, args);
+    ::OutputDebugStringA(buf);
+#else
+    vprintf(fmt, args);
+#endif
+    va_end(args);
+}
+
+void Print(const wchar_t *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+#ifdef _WIN32
+    wchar_t buf[1024];
+    _vsnwprintf(buf, sizeof(buf), fmt, args);
+    ::OutputDebugStringW(buf);
+#else
+    vwprintf(fmt, args);
+#endif
+    va_end(args);
+}
+
+} // namespace mu
--- Plugin~/Src/MeshUtils/muLog.h
+++ Plugin~/Src/MeshUtils/muLog.h
@@ -1,10 +1,10 @@
 #pragma once
 
-namespace mu {
-
 #define muLogInfo(...)    ::mu::Print("MeshSync info: " __VA_ARGS__)
 #define muLogWarning(...) ::mu::Print("MeshSync warning: " __VA_ARGS__)
 #define muLogError(...)   ::mu::Print("MeshSync error: " __VA_ARGS__)
+
+namespace mu {
 
 void Print(const char *fmt, ...);
 void Print(const wchar_t *fmt, ...);
