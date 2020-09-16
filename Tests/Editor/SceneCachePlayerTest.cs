e146abb723408ba66b1c76689d14e455826e2cc7
sindharta <sindharta@gmail.com>
2020-09-16 16:34:15 +0900

    Add a test for testing the creation of SceneCachePlayer

---
+++ Tests/Editor/SceneCachePlayerTest.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+
+namespace Unity.MeshSync.Editor.Tests {
+
+
+public class SceneCachePlayerTest  {
+
+    [Test]
+    public void CreateSceneCache() {
+
+        string cubeTestDataPath = Path.Combine(TEST_DATA_PATH, "Cube.sc");
+        string sphereTestDataPath = Path.Combine(TEST_DATA_PATH, "Sphere.sc");
+
+        CreateAndDeleteSceneCachePlayerPrefab(cubeTestDataPath);
+        CreateAndDeleteSceneCachePlayerPrefab(sphereTestDataPath);
+        
+    }
+
+//----------------------------------------------------------------------------------------------------------------------    
+
+    private void CreateAndDeleteSceneCachePlayerPrefab(string sceneCachePath) {
+        Assert.IsTrue(File.Exists(sceneCachePath));
+        
+        
+        GameObject go = SceneCachePlayerEditor.CreateSceneCachePlayerPrefab(sceneCachePath);
+        Assert.IsNotNull(go);
+        GameObject prefab = PrefabUtility.GetCorrespondingObjectFromSource(go);
+        Assert.IsNotNull(prefab);
+        string prefabPath = AssetDatabase.GetAssetPath(prefab);
+        Assert.IsFalse(string.IsNullOrEmpty(prefabPath));
+
+        string prefabAssetsFolder = Path.Combine(
+            Path.GetDirectoryName(prefabPath),
+            Path.GetFileNameWithoutExtension(prefabPath)
+        );
+        
+        Assert.IsFalse(string.IsNullOrEmpty(prefabAssetsFolder));       
+        
+        string[] prefabAssetGUIDs = AssetDatabase.FindAssets("", new[] {prefabAssetsFolder});
+        foreach (string guid in prefabAssetGUIDs) {
+            AssetDatabase.DeleteAsset(AssetDatabase.GUIDToAssetPath(guid));
+        }
+
+        FileUtil.DeleteFileOrDirectory(prefabAssetsFolder);
+        
+        //Cleanup
+        Object.DestroyImmediate(go);
+        AssetDatabase.DeleteAsset(prefabPath);
+
+        AssetDatabase.Refresh();
+        
+    }
+
+//----------------------------------------------------------------------------------------------------------------------    
+    
+    private readonly string TEST_DATA_PATH = Path.Combine("Packages", MeshSyncConstants.PACKAGE_NAME, "Tests", "Data");
+
+}
+
+}
