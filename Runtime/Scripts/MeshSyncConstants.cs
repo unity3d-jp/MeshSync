4ba364c2bab2e0648984444ef285f19c60e56d8a
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2020-09-18 13:09:53 +0900

    Revert "test: add a test for creating SceneCachePlayer (#230)"
    This reverts commit e01c0803750fbf54c4a27205ef364590024e851b.

--- Runtime/Scripts/MeshSyncConstants.cs
+++ Runtime/Scripts/MeshSyncConstants.cs
@@ -4,9 +4,6 @@ internal static class MeshSyncConstants {
 
     internal const int DEFAULT_SERVER_PORT = 8080;
     
-    internal const string PACKAGE_NAME = "com.unity.meshsync";
-
-    
 }    
     
 } //end namespace
