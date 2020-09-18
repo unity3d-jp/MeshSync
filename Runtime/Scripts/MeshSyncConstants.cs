e01c0803750fbf54c4a27205ef364590024e851b
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2020-09-18 13:07:14 +0900

    test: add a test for creating SceneCachePlayer (#230)
    * Add SceneCache test data
    * Add a test for testing the creation of SceneCachePlayer
    * Make sure that SceneCache files are treated as binary in git
    * Ignore automatic import of SceneCache files outside Assets folder

--- Runtime/Scripts/MeshSyncConstants.cs
+++ Runtime/Scripts/MeshSyncConstants.cs
@@ -4,6 +4,9 @@ internal static class MeshSyncConstants {
 
     internal const int DEFAULT_SERVER_PORT = 8080;
     
+    internal const string PACKAGE_NAME = "com.unity.meshsync";
+
+    
 }    
     
 } //end namespace
