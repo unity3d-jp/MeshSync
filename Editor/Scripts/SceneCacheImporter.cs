9dc60c0de3a625b04a6187be2fc906d05d65384e
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2020-09-18 13:10:41 +0900

    revert: "test: add a test for creating SceneCachePlayer (#230)" (#235)
    This reverts commit e01c0803750fbf54c4a27205ef364590024e851b.

--- Editor/Scripts/SceneCacheImporter.cs
+++ Editor/Scripts/SceneCacheImporter.cs
@@ -9,18 +9,15 @@ namespace Unity.MeshSync.Editor
     [ScriptedImporter(1, "sc")]
     internal class SceneCacheImporter : ScriptedImporter
     {
-        public override void OnImportAsset(AssetImportContext ctx) {
-            //Ignore assets outside Assets folder
-            string path = ctx.assetPath;
-            if (!path.StartsWith(Application.dataPath))
-                return;
-            
-            GameObject go = SceneCachePlayerEditor.CreateSceneCachePlayer(path);
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            var path = ctx.assetPath;
+            var go = SceneCachePlayerEditor.CreateSceneCachePlayer(path);
             if (go == null)
                 return;
 
             // export materials & animation and generate prefab
-            SceneCachePlayer player = go.GetComponent<SceneCachePlayer>();
+            var player = go.GetComponent<SceneCachePlayer>();
             player.UpdatePlayer();
             player.ExportMaterials(false, true);
             player.ResetTimeAnimation();
