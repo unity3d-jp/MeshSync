32608b528e91030529456e9c92037ddf97e1b4af
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2020-09-24 12:08:11 +0900

    chore: split MeshSyncPlayer into several files (#247)
    * Split AudioHolder, EntityRecord, MaterialHolder, TextureHolder to different files

---
+++ Runtime/Scripts/PlayerData/EntityRecord.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+namespace Unity.MeshSync {
+
+[Serializable]
+internal class EntityRecord {
+
+    // return true if modified
+    public bool BuildMaterialData(MeshData md) {
+        int numSubmeshes = md.numSubmeshes;
+
+        bool updated = false;
+        if (materialIDs == null || materialIDs.Length != numSubmeshes) {
+            materialIDs = new int[numSubmeshes];
+            updated = true;
+        }
+        
+        for (int i = 0; i < numSubmeshes; ++i) {
+            int mid = md.GetSubmesh(i).materialID;
+            if (!updated)
+                updated = materialIDs[i] != mid;
+            materialIDs[i] = mid;
+        }
+        return updated;
+    }
+    
+//----------------------------------------------------------------------------------------------------------------------
+    
+    public EntityType          dataType;
+    public int                 index;
+    public GameObject          go;
+    public Transform           trans;
+    public Camera              camera;
+    public Light               light;
+    public MeshFilter          meshFilter;
+    public MeshRenderer        meshRenderer;
+    public SkinnedMeshRenderer skinnedMeshRenderer;
+    public PointCache          pointCache;
+    public PointCacheRenderer  pointCacheRenderer;
+    public Mesh                origMesh;
+    public Mesh                mesh;
+
+    public int[]           materialIDs;
+    public string          reference;
+    public string          rootBonePath;
+    public string[]        bonePaths;
+    public bool            smrUpdated    = false;
+    public bool            smrEnabled    = false;
+    public bool            hasVisibility = false;
+    public VisibilityFlags visibility; // for reference
+    public bool            recved = false;
+    
+}
+
+
+} //end namespace
---
+++ Runtime/Scripts/PlayerData/MaterialHolder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Unity.MeshSync {
+
+[Serializable]
+internal class MaterialHolder {
+    public int      id;
+    public string   name;
+    public int      index;
+    public string   shader;
+    public Color    color = Color.white;
+    public Material material;
+    public int      materialIID;
+}
+
+} //end namespace
