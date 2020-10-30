530c69b8896b2b9a3242bd7b393ae253c4ae7b7e
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2020-10-30 12:39:22 +0900

    feat: notify users to restart Unity after upgrading MeshSync for Unity 2020.2+ (#278)
    * Add a notifier to request user to restart the editor when the user updates MeshSync package
    
    Others:
    * Update package dependency to com.unity.anime-toolbox@0.3.0-preview
    * Update package dependency to com.unity.sharp-zip-lib@1.2.2-preview.1
    * Update dependencies in the test project

---
+++ Editor/Scripts/PackageEventSubscriber.cs
@@ -0,0 +1,37 @@
+﻿#if UNITY_2020_2
+
+using Unity.AnimeToolbox.Editor;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+
+
+namespace Unity.MeshSync.Editor {
+
+internal static class PackageEventSubscriber {
+
+    [InitializeOnLoadMethod]
+    private static void PackageEventSubscriber_OnEditorLoad() {
+        Events.registeredPackages += OnPackagesRegistered;
+    }
+    
+//----------------------------------------------------------------------------------------------------------------------    
+    
+    static void OnPackagesRegistered(PackageRegistrationEventArgs packageRegistrationEventArgs) {
+       
+        PackageInfo curPackage = packageRegistrationEventArgs.removed.FindPackage(MeshSyncConstants.PACKAGE_NAME);
+
+        if (null == curPackage) {
+            curPackage = packageRegistrationEventArgs.changedTo.FindPackage(MeshSyncConstants.PACKAGE_NAME);
+        }
+
+        if (null == curPackage) {
+            return;
+        }        
+        EditorRestartMessageNotifier.RequestNotificationOnLoad(curPackage);                    
+    }
+}
+
+} //end namespace
+
+#endif //UNITY_2020_2
