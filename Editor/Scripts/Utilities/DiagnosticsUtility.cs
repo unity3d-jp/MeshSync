6d0a116826ba81d06760e972551dde4e5c1d3535
sindharta <sindharta@gmail.com>
2020-11-12 15:24:10 +0900

    Add diagnosticsUtility

---
+++ Editor/Scripts/Utilities/DiagnosticsUtility.cs
@@ -0,0 +1,22 @@
+﻿namespace Unity.MeshSync.Editor  {
+
+internal static class DiagnosticsUtility {
+
+    internal static void StartProcess(string appPath, bool useShellExecute = true, bool redirectStandardError = false, 
+        string arguments = "") {
+
+        System.Diagnostics.Process process = new System.Diagnostics.Process {
+            StartInfo = {
+                FileName              = appPath,
+                UseShellExecute       = useShellExecute,
+                RedirectStandardError = redirectStandardError,
+                Arguments             = arguments,    
+            },
+            EnableRaisingEvents = true
+        };            
+        process.Start();            
+    }
+
+}
+
+} //end namespace
