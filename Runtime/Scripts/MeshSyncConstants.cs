454ce46f039f01ff36b30c5994ee3f453b4aae70
sindharta <sindharta@gmail.com>
2020-12-02 20:49:02 +0900

    Change the default path of SceneCacheResources

--- Runtime/Scripts/MeshSyncConstants.cs
+++ Runtime/Scripts/MeshSyncConstants.cs
@@ -3,7 +3,7 @@
 internal static class MeshSyncConstants {
 
     internal const int     DEFAULT_SERVER_PORT = 8080;
-    internal const string  DEFAULT_SCENE_CACHE_OUTPUT_PATH  = "Assets/SceneCache";
+    internal const string  DEFAULT_SCENE_CACHE_OUTPUT_PATH  = "Assets/SceneCacheResources";
     
     internal const string PACKAGE_NAME = "com.unity.meshsync";
     
