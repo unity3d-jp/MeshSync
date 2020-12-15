2e52dbf3f834dcbae3a6ed3a217b46f168232aaa
sindharta <sindharta@gmail.com>
2020-12-15 15:18:42 +0900

    fix compile error on 2020.2 and above

--- Editor/Scripts/SceneCacheImporter.cs
+++ Editor/Scripts/SceneCacheImporter.cs
@@ -1,6 +1,11 @@
 using System.IO;
 using UnityEngine;
+
+#if UNITY_2020_2_OR_NEWER
+using UnityEditor.AssetImporters;
+#else
 using UnityEditor.Experimental.AssetImporters;
+#endif
 
 
 namespace Unity.MeshSync.Editor {
