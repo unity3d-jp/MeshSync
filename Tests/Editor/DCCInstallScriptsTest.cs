20bd3fd673aa03ef0c678060b2354134c62e256d
sindharta <sindharta@gmail.com>
2020-12-16 11:55:04 +0900

    add test to check 3dsMax install scripts

--- Tests/Editor/DCCInstallScriptsTest.cs
+++ Tests/Editor/DCCInstallScriptsTest.cs
@@ -11,19 +11,8 @@ namespace Unity.MeshSync.Editor.Tests {
 internal class DCCInstallScriptsTest {
     [Test]
     public void CheckSupportedBlenderInstallScripts() {
-        //Create a list containing unique versions of Blender
-        HashSet<string> versions = new HashSet<string>();
-        foreach (var dccToolInfo in MeshSyncEditorConstants.DEFAULT_DCC_TOOLS_BY_FOLDER) {
-            if (dccToolInfo.Value.Type != DCCToolType.BLENDER)
-                continue;
-
-            string curVer = dccToolInfo.Value.DCCToolVersion;
-            if (versions.Contains(curVer) || string.IsNullOrEmpty(curVer))
-                continue;
-            
-            versions.Add(curVer);
-        }
-
+        HashSet<string> versions = FindUniqueVersionsOfDefaultDCCTools(DCCToolType.BLENDER);
+        
         foreach (string ver in versions) {
             string installScriptTemplatePath = BlenderIntegrator.GetInstallScriptTemplatePath(ver);
             Assert.IsFalse(string.IsNullOrEmpty(installScriptTemplatePath), 
@@ -37,6 +26,37 @@ internal class DCCInstallScriptsTest {
 
     }
 
+//----------------------------------------------------------------------------------------------------------------------
+    
+    [Test]
+    public void CheckSupported3DSMaxInstallScripts() {
+        HashSet<string> versions = FindUniqueVersionsOfDefaultDCCTools(DCCToolType.AUTODESK_3DSMAX);
+        
+        foreach (string ver in versions) {
+            string installScriptTemplatePath = _3DSMaxIntegrator.GetInstallScriptTemplatePath(ver);
+            Assert.IsFalse(string.IsNullOrEmpty(installScriptTemplatePath), 
+                $"Install script Template for 3DSMax {ver} is missing");
+        }
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+    
+    HashSet<string> FindUniqueVersionsOfDefaultDCCTools(DCCToolType dccToolType) {
+        HashSet<string> versions = new HashSet<string>();
+        foreach (var dccToolInfo in MeshSyncEditorConstants.DEFAULT_DCC_TOOLS_BY_FOLDER) {
+            if (dccToolInfo.Value.Type != dccToolType)
+                continue;
+
+            string curVer = dccToolInfo.Value.DCCToolVersion;
+            if (versions.Contains(curVer) || string.IsNullOrEmpty(curVer))
+                continue;
+            
+            versions.Add(curVer);
+        }
+
+        return versions;
+    }
+    
 }
 //----------------------------------------------------------------------------------------------------------------------    
 
