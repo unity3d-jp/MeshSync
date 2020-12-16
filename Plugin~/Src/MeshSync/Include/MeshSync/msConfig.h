5d8641830ebe65893d7bd1ae00ec9887e6938293
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2020-12-16 14:49:40 +0900

    chore: creating 0.6.1-preview release (#310)
    * update package.json and changelog
    * update msconfig
    * Update Windows DLL, Mac bundle, and Linux so file

--- Plugin~/Src/MeshSync/Include/MeshSync/msConfig.h
+++ Plugin~/Src/MeshSync/Include/MeshSync/msConfig.h
@@ -1,7 +1,7 @@
 #pragma once
 
 //Note: Every update to the plugin must increase the version number
-#define msPluginVersionStr "0.6.0-preview"
+#define msPluginVersionStr "0.6.1-preview"
 #define msVendor "Unity Technologies"
 #define msProtocolVersion 123
 
