3ebff20c6510722808c5e80576e1ff98ec1559b8
sindharta <sindharta@gmail.com>
2021-01-13 21:34:31 +0900

    remove unnecessary code

--- Runtime/Scripts/SceneCache/SceneCacheTrack.cs
+++ Runtime/Scripts/SceneCache/SceneCacheTrack.cs
@@ -1,8 +1,4 @@
-﻿using UnityEngine;
-using UnityEngine.Assertions;
-using UnityEngine.Playables;
-using UnityEngine.Timeline;
-using UnityEngine.UI;
+﻿using UnityEngine.Timeline;
 
 namespace Unity.MeshSync {
 
@@ -10,19 +6,7 @@ namespace Unity.MeshSync {
 [TrackColor(0.263f, 0.09f, 0.263f)]
 [NotKeyable]
 internal class SceneCacheTrack : TrackAsset {
-    // public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount) {
-    //
-    //     ScriptPlayable<SceneCachePlayableMixer> mixerScriptPlayable = ScriptPlayable<SceneCachePlayableMixer>.Create(graph, inputCount);
-    //     PlayableDirector director = go.GetComponent<PlayableDirector>();
-    //     Assert.IsNotNull(director);
-    //     
-    //     //Initialize mixer
-    //     SceneCachePlayableMixer mixer = mixerScriptPlayable.GetBehaviour();
-    //     mixer.Init(director, GetClips());
-    //     
-    //     
-    //     return mixerScriptPlayable;
-    // }
+
 }
 
 }
