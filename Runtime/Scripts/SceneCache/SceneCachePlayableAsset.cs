87467e0d5e33ed3de4980ca165c5fcce74115268
sindharta <sindharta@gmail.com>
2021-01-14 15:16:10 +0900

    Store PlayableBehaviour in SceneCachePlayableAsset

--- Runtime/Scripts/SceneCache/SceneCachePlayableAsset.cs
+++ Runtime/Scripts/SceneCache/SceneCachePlayableAsset.cs
@@ -16,15 +16,36 @@ internal class SceneCachePlayableAsset : PlayableAsset, ITimelineClipAsset {
 
     public override Playable CreatePlayable(PlayableGraph graph, GameObject go) {
         
-        SceneCachePlayableBehaviour bh = new SceneCachePlayableBehaviour();
-        bh.SetSceneCachePlayer(m_sceneCachePlayerRef.Resolve(graph.GetResolver()));
-        return ScriptPlayable<SceneCachePlayableBehaviour>.Create(graph, bh);
+        m_sceneCachePlayableBehaviour = new SceneCachePlayableBehaviour();
+        m_sceneCachePlayableBehaviour.SetSceneCachePlayer(m_sceneCachePlayerRef.Resolve(graph.GetResolver()));
+        return ScriptPlayable<SceneCachePlayableBehaviour>.Create(graph, m_sceneCachePlayableBehaviour);
     }
-   
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    internal float GetNormalizedTime() {
+        if (null == m_sceneCachePlayableBehaviour) {
+            return 0;            
+        }
+
+        SceneCachePlayer sc = m_sceneCachePlayableBehaviour.GetSceneCachePlayer();
+        if (null == sc)
+            return 0;
+        
+        return sc.GetNormalizedTime();
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    internal ExposedReference<SceneCachePlayer> GetSceneCachePlayerRef() { return m_sceneCachePlayerRef;}
+    
 //----------------------------------------------------------------------------------------------------------------------
     
-    [SerializeField] private ExposedReference<SceneCachePlayer>  m_sceneCachePlayerRef = new ExposedReference<SceneCachePlayer>();
+    private ExposedReference<SceneCachePlayer>  m_sceneCachePlayerRef;
 
+    
+    SceneCachePlayableBehaviour m_sceneCachePlayableBehaviour = null;
+    
 }
 
 
