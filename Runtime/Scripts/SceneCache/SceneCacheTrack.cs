a4e147c7727bd231f83d958b2b79a3725a81de9c
sindharta <sindharta@gmail.com>
2021-01-18 11:44:50 +0900

    Remove Notkeyable from SceneCacheTrack

--- Runtime/Scripts/SceneCache/SceneCacheTrack.cs
+++ Runtime/Scripts/SceneCache/SceneCacheTrack.cs
@@ -4,7 +4,6 @@ namespace Unity.MeshSync {
 
 [TrackClipType(typeof(SceneCachePlayableAsset))]
 [TrackColor(0.196f, 0.804f, 0.196f)] //limegreen
-[NotKeyable]
 internal class SceneCacheTrack : TrackAsset {
 
 }
