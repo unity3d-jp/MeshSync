82ba720d9a124035e45d2b79313445e6a6f7b87d
sindharta <sindharta@gmail.com>
2021-01-21 15:07:40 +0900

    minor

--- Runtime/Scripts/TimelineClipData/BaseTimelineClipData.cs
+++ Runtime/Scripts/TimelineClipData/BaseTimelineClipData.cs
@@ -38,13 +38,13 @@ internal class BaseTimelineClipData : ISerializationCallbackReceiver {
     [NonSerialized] private TimelineClip  m_clipOwner = null;
 
 #pragma warning disable 414    
-    [HideInInspector][SerializeField] private int m_version = CUR_TIMELINE_CLIP_SIS_DATA_VERSION;        
+    [HideInInspector][SerializeField] private int m_version = CUR_TIMELINE_CLIP_DATA_VERSION;        
 #pragma warning restore 414    
     
 
     private       bool   m_frameMarkersVisibility           = false;
     
-    private const int    CUR_TIMELINE_CLIP_SIS_DATA_VERSION = 1;
+    private const int    CUR_TIMELINE_CLIP_DATA_VERSION = 1;
     
 }
 
