0bd35229046ae0556f58855bb94a9f30bc34b72b
sindharta <sindharta@gmail.com>
2021-01-22 16:43:36 +0900

    SetAnimationCurve to SceneCacheClipData when the sceneCachePlayer is bound to the TimelineClip

--- Runtime/Scripts/SceneCache/SceneCacheClipData.cs
+++ Runtime/Scripts/SceneCache/SceneCacheClipData.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using Unity.FilmInternalUtilities;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Timeline;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Timeline;
+#endif
 
 namespace Unity.MeshSync {
 
@@ -11,17 +18,75 @@ internal class SceneCacheClipData : BaseClipData {
     
     internal void BindSceneCachePlayer(SceneCachePlayer sceneCachePlayer) {
         if (sceneCachePlayer == m_scPlayer) {
-            Debug.Log("SceneCache is already found");
             return;
         }
 
-        m_scPlayer = sceneCachePlayer;
-        Debug.Log("SceneCache successfully bound");
+        m_scPlayer         = sceneCachePlayer;
+        m_animationCurve   = sceneCachePlayer.GetOrigAnimationCurve();
+        UpdateClipAnimationCurve();
+        
     }
 
     internal void UnbindSceneCachePlayer() {
+        
         m_scPlayer = null;
-        Debug.Log("SceneCache successfully UN-bound");
+        
+        TimelineClip clip = GetOwner();
+        Assert.IsNotNull(clip);
+        m_animationCurve = AnimationCurve.Linear(0, 0,(float) clip.duration,0 );
+        UpdateClipAnimationCurve();
+    }
+    
+//----------------------------------------------------------------------------------------------------------------------
+    void UpdateClipAnimationCurve() {
+        TimelineClip clip = GetOwner();
+        Assert.IsNotNull(clip);
+
+        bool shouldRefresh = false;
+        
+#if UNITY_EDITOR        
+        AnimationCurve shownCurve = AnimationUtility.GetEditorCurve(clip.curves, m_timeCurveBinding);
+        shouldRefresh = !CurveApproximately(shownCurve, m_animationCurve); 
+#endif
+        
+        clip.curves.SetCurve("", typeof(SceneCachePlayableAsset), "m_time", m_animationCurve);
+        
+#if UNITY_EDITOR        
+        if (shouldRefresh) {
+            TimelineEditor.Refresh(RefreshReason.ContentsAddedOrRemoved );            
+        }
+#endif
+        
+        
+    }
+
+    static bool CurveApproximately(AnimationCurve x, AnimationCurve y) {
+        if (null == x && null == y)
+            return true;
+        
+        if (null == x || null == y)
+            return false;
+
+        Keyframe[] xKeys = x.keys;
+        Keyframe[] yKeys = y.keys;
+
+        if (xKeys.Length != yKeys.Length)
+            return false;
+
+        int lastIndex = xKeys.Length-1;
+        
+        //only check first and last frame
+        if ( KeyframeApproximately(xKeys[0],yKeys[0]) && KeyframeApproximately(xKeys[lastIndex],yKeys[lastIndex])) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    static bool KeyframeApproximately(Keyframe k0, Keyframe k1) {
+        //only check time and value
+        return Mathf.Approximately(k0.time, k1.time) && Mathf.Approximately(k0.value, k1.value);
     }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -38,6 +103,12 @@ internal class SceneCacheClipData : BaseClipData {
     [SerializeField] private AnimationCurve m_animationCurve;
 
 
+    private static EditorCurveBinding m_timeCurveBinding =  
+        new EditorCurveBinding() {
+            path         = "",
+            type         = typeof(SceneCachePlayableAsset),
+            propertyName = "m_time"
+        };    
 }
 
 
