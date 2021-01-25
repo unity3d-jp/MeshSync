001fff58067df68f369a05c0e1610d2f7d127a62
sindharta <sindharta@gmail.com>
2021-01-25 19:33:38 +0900

    Create the inspector for SceneCachePlayableAsset

---
+++ Editor/Scripts/SceneCache/SceneCachePlayableAssetInspector.cs
@@ -0,0 +1,38 @@
+﻿using Unity.AnimeToolbox;
+using UnityEditor;
+using UnityEngine;
+
+namespace Unity.MeshSync.Editor {
+
+[CustomEditor(typeof(SceneCachePlayableAsset))]
+internal class SceneCachePlayableAssetInspector : UnityEditor.Editor {
+
+    void OnEnable() {
+        m_scPlayableAsset = target as SceneCachePlayableAsset;
+    }
+    
+    void OnDisable() {
+        m_scPlayableAsset = null;
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+    public override void OnInspectorGUI() {
+        if (m_scPlayableAsset.IsNullRef())
+            return;
+        
+        SerializedObject so = serializedObject;
+
+        EditorGUILayout.PropertyField(so.FindProperty("m_sceneCachePlayerRef"),
+            new GUIContent("SceneCachePlayerRef"));
+        
+        so.ApplyModifiedProperties();
+       
+    }
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    private SceneCachePlayableAsset m_scPlayableAsset;
+
+}
+} //end namespace
