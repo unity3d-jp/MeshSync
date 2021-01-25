59c9d332ed8b99e61ec0a04c9f2e59fe72ad0d5e
sindharta-tanuwijaya <71803280+sindharta-tanuwijaya@users.noreply.github.com>
2021-01-25 17:31:02 +0900

    chore: separate ClientSettings from msClient in the plugin code (#326)
    * separate ClientSettings from msClient in the plugin code

--- Plugin~/Src/MeshSync/Include/MeshSync/msClient.h
+++ Plugin~/Src/MeshSync/Include/MeshSync/msClient.h
@@ -1,15 +1,10 @@
 #pragma once
 
 #include "msProtocol.h"
+#include "msClientSettings.h" //ClientSettings
 
 namespace ms {
 
-struct ClientSettings
-{
-    std::string server = "127.0.0.1";
-    uint16_t port = 8080;
-    int timeout_ms = 30000;
-};
 
 class Client
 {
---
+++ Plugin~/Src/MeshSync/Include/MeshSync/msClientSettings.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace ms {
+
+struct ClientSettings {
+    std::string server = "127.0.0.1";
+    uint16_t port = 8080;
+    int timeout_ms = 30000;
+};
+
+} // namespace ms
