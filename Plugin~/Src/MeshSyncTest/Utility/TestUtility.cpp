48b7ca377a8e3880c2b1a137aaa8ed4023a2e076
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2021-07-01 22:02:41 +0900

    fix-plugin: fix compile error when building the plugin in test mode (#362)

--- Plugin~/Src/MeshSyncTest/Utility/TestUtility.cpp
+++ Plugin~/Src/MeshSyncTest/Utility/TestUtility.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "TestUtility.h"
 #include "Test.h"               //GetArg
-#include "MeshSync/Utility/msAsyncSceneExporter.h" //ms::AsyncSceneSender
+#include "MeshSync/AsyncSceneSender.h" //ms::AsyncSceneSender
 
 ms::ClientSettings TestUtility::GetClientSettings(){
     ms::ClientSettings ret;
