0908b8427cfef7c4f6f552b5f2cf2eeb9159673d
sindharta <sindharta@gmail.com>
2021-07-01 10:24:28 +0900

    refactor-plugin: remove noncopyable.
    Using noncopyable will require a class to have multiple parents, if that class already has an original parent, and our policy is to avoid using multiple inheritance.
    Therefore, this class won't be used

--- Plugin~/Src/MeshUtils/Include/MeshUtils/muMisc.h
+++ Plugin~/Src/MeshUtils/Include/MeshUtils/muMisc.h
@@ -40,16 +40,6 @@ void InitializeSymbols(const char *path = nullptr);
 void* FindSymbolByName(const char *name);
 void* FindSymbolByName(const char *name, const char *module_name);
 
-
-struct noncopyable
-{
-    noncopyable() {}
-    noncopyable(noncopyable&&) {}
-    noncopyable(const noncopyable&) = delete;
-    noncopyable& operator=(const noncopyable&) = delete;
-};
-
-
 enum class MemoryFlags
 {
     ExecuteRead,
