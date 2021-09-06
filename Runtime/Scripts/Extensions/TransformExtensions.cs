dd6e34ef3fcc6cc5768f91ce6e274c379014770e
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-09-06 18:09:42 +0900

    Add TransformExtensions

---
+++ Runtime/Scripts/Extensions/TransformExtensions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Unity.MeshSync {
+
+internal static class TransformExtensions {
+
+    //[TODO-sin: 2021-9-6] Move to FIU
+    internal static Transform FindOrCreate(this Transform t, string childName, bool worldPositionStays = true) {
+        
+        Transform childT = t.Find(childName);
+        if (null != childT)
+            return childT;
+        
+        
+        GameObject go = new GameObject { name = childName };
+        childT = go.transform;
+        childT.SetParent(t, worldPositionStays);
+        return childT;
+    }
+    
+
+
+    
+}
+
+} //end namespace
