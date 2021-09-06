c3072d9a2687c58d7fa018a5b1fa6d316718739d
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-09-06 17:39:40 +0900

    Simplify

--- Runtime/Scripts/Utilities/ObjectUtility.cs
+++ Runtime/Scripts/Utilities/ObjectUtility.cs
@@ -7,18 +7,16 @@ namespace Unity.MeshSync {
 internal static class ObjectUtility {
 
     //[TODO-sin: 2021-9-6] Move to FIU
-    internal static Transform FindFirstRootGameObject(string objectName) {
-        Transform ret = null;
+    internal static GameObject FindFirstRootGameObject(string objectName) {
 
         GameObject[] roots = SceneManager.GetActiveScene().GetRootGameObjects();
         foreach (GameObject go in roots) {
             if (go.name != objectName) 
                 continue;
-                
-            ret = go.GetComponent<Transform>();
-            break;
+
+            return go;
         }
-        return ret;
+        return null;
     }
     
 }
