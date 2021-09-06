54a913df167f70702e9fa2f65aa55ef90b9ad904
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-09-06 17:56:41 +0900

    Add FindByPath

--- Runtime/Scripts/Utilities/ObjectUtility.cs
+++ Runtime/Scripts/Utilities/ObjectUtility.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,17 +8,51 @@ namespace Unity.MeshSync {
 internal static class ObjectUtility {
 
     //[TODO-sin: 2021-9-6] Move to FIU
-    internal static GameObject FindFirstRootGameObject(string objectName) {
+    [CanBeNull]
+    internal static Transform FindFirstRootGameObject(string objectName) {
 
         GameObject[] roots = SceneManager.GetActiveScene().GetRootGameObjects();
         foreach (GameObject go in roots) {
             if (go.name != objectName) 
                 continue;
 
-            return go;
+            return go.transform;
         }
         return null;
     }
+    
+
+    // returns null if not found
+    private static Transform FindByPath(Transform parent, string path) {
+        string[] names = path.Split('/');
+        if (names.Length <= 0)
+            return null;
+
+        //if parent is null, search from root game objects
+        Transform t = parent;
+        int       tokenStartIdx = 0;
+        if (null == t) {
+            t = FindFirstRootGameObject(names[0]);
+            if (null == t)
+                return t;
+
+            tokenStartIdx = 1;
+        }
+
+        //loop
+        int nameLength = names.Length;
+        for (int i = tokenStartIdx; i < nameLength; ++i) {
+            string nameToken = names[i];
+            if (string.IsNullOrEmpty(nameToken))
+                continue;
+
+            t = t.Find(nameToken);
+        }
+
+        return t;        
+    }
+    
+    
     
 }
 
