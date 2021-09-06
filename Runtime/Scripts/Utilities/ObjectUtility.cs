97b5f7b38f33f61722f7bec593a837299b3201ab
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-09-06 17:38:35 +0900

    Add ObjectUtility

---
+++ Runtime/Scripts/Utilities/ObjectUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Unity.MeshSync {
+
+internal static class ObjectUtility {
+
+    //[TODO-sin: 2021-9-6] Move to FIU
+    internal static Transform FindFirstRootGameObject(string objectName) {
+        Transform ret = null;
+
+        GameObject[] roots = SceneManager.GetActiveScene().GetRootGameObjects();
+        foreach (GameObject go in roots) {
+            if (go.name != objectName) 
+                continue;
+                
+            ret = go.GetComponent<Transform>();
+            break;
+        }
+        return ret;
+    }
+    
+}
+
+} //end namespace
