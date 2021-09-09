c534e6670b62c730fa9dacad667e135029a025bb
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-09-09 11:16:38 +0900

    Apply Snap To Frame in the calculation

--- Runtime/Scripts/SceneCache/SceneCachePlayableBehaviour.cs
+++ Runtime/Scripts/SceneCache/SceneCachePlayableBehaviour.cs
@@ -16,6 +16,7 @@ internal class SceneCachePlayableBehaviour : PlayableBehaviour {
     }
 
     internal void SetClipData(SceneCacheClipData clipData) { m_clipData = clipData; } 
+    internal void SetSnapToFrame(SnapToFrame snap) { m_snapToFrame = snap; } 
     
 //----------------------------------------------------------------------------------------------------------------------        
     
@@ -24,19 +25,36 @@ internal class SceneCachePlayableBehaviour : PlayableBehaviour {
         if (m_sceneCachePlayer.IsNullRef()) {
             return;
         }
-                
         AnimationCurve curve = m_clipData.GetAnimationCurve();
-        float normalizedTime = curve.Evaluate((float) playable.GetTime());
+
+        float normalizedTime = 0;
+        switch (m_snapToFrame) {
+            case SnapToFrame.NONE: {
+                normalizedTime = curve.Evaluate((float) playable.GetTime());
+                break;                 
+            }
+            case SnapToFrame.NEAREST: {
+
+                float fps = (float) m_clipData.GetOwner().GetParentTrack().timelineAsset.editorSettings.GetFPS();
+                
+                float timePerFrame = 1.0f / fps;
+                int   frame        = Mathf.RoundToInt((float)playable.GetTime() * fps);
+                normalizedTime = curve.Evaluate(frame * timePerFrame);
+                break;
+            }            
+        }
+              
         m_sceneCachePlayer.RequestNormalizedTime(normalizedTime);
 
     }
 
     
-//----------------------------------------------------------------------------------------------------------------------        
+//----------------------------------------------------------------------------------------------------------------------
+    
     private SceneCachePlayer m_sceneCachePlayer = null;
     
     private SceneCacheClipData m_clipData = null;
-    
+    private SnapToFrame        m_snapToFrame;
 }
 
 } //end namespace
