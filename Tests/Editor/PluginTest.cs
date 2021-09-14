8183375906ba8b22a7d362c462dab395da09020c
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-09-14 19:25:13 +0900

    test

--- Tests/Editor/PluginTest.cs
+++ Tests/Editor/PluginTest.cs
@@ -6,7 +6,7 @@ using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 
 namespace Unity.MeshSync.Editor.Tests {
-public class PluginTest {
+internal class PluginTest {
     
     [UnityTest]
     public IEnumerator CheckPluginVersion() {
@@ -35,6 +35,63 @@ public class PluginTest {
         }
         
     }
+    
+//----------------------------------------------------------------------------------------------------------------------
+    
+    /// <summary>
+    /// Parse a semantic versioned string to a PackageVersion class
+    /// </summary>
+    /// <param name="semanticVer">Semantic versioned input string</param>
+    /// <param name="packageVersion">The detected PackageVersion. Set to null when the parsing fails</param>
+    /// <returns>true if successful, false otherwise</returns>
+    public static bool TryParseMajorAndMinorVersion(string semanticVer, out PackageVersion packageVersion) {
+        packageVersion = null;
+        string[] tokens = semanticVer.Split('.');
+        if (tokens.Length <= 2)
+            return false;
+
+        if (!int.TryParse(tokens[0], out int major))
+            return false;
+
+        if (!int.TryParse(tokens[1], out int minor))
+            return false;
+
+        //Find patch and lifecycle
+        string[] patches = tokens[2].Split('-');
+        if (!int.TryParse(patches[0], out int patch))
+            return false;
+               
+        PackageLifecycle lifecycle = PackageLifecycle.INVALID;
+        if (patches.Length > 1) {
+            string lifecycleStr = patches[1].ToLower();                    
+            switch (lifecycleStr) {
+                case "experimental": lifecycle = PackageLifecycle.EXPERIMENTAL; break;
+                case "preview"     : lifecycle = PackageLifecycle.PREVIEW; break;
+                case "pre"         : lifecycle = PackageLifecycle.PRERELEASE; break;
+                default: lifecycle             = PackageLifecycle.INVALID; break;
+            }
+            
+        } else {
+            lifecycle = PackageLifecycle.RELEASED; 
+            
+        }
+
+        packageVersion = new PackageVersion() {
+            Major     = major,
+            Minor     = minor,
+            Patch     = patch,
+            Lifecycle = lifecycle
+        };
+
+        const int METADATA_INDEX = 3;
+        if (tokens.Length > METADATA_INDEX) {
+            packageVersion.AdditionalMetadata = String.Join(".",tokens, METADATA_INDEX, tokens.Length-METADATA_INDEX);
+        }
+
+        return true;
+
+    } 
+    
 }
 
 } //end namespace
