32fbfa1fcf78a1bb89e0d711a38feade344d28cb
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-09-29 15:23:09 +0900

    comment

--- Runtime/Scripts/MeshSyncCallbacks.cs
+++ Runtime/Scripts/MeshSyncCallbacks.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Unity.MeshSync {
 
+/// <summary>
+/// A callback that is called when receiving message data from server 
+/// </summary>
 public delegate void ServerMessageCallback(MessageType type, IntPtr data);
 
 } //end namespace
