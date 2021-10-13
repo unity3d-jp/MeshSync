31156a8e0aaf0d50f9d29dc7aedd69e2427d9435
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2021-10-13 22:28:06 +0900

    project: add HDRP test project (#428)
    * Initial HDRP project creation
    * add missing meta
    * add packages-lock.json
    * add dummy com.unity.meshsync in the HDRP project
    * update packages-lock.json
    * add MeshSyncServer StreamingAssets files
    * add sample SceneCache
    * delete HDRP ExampleAssets
    
    
    chore:
    * update sync script
    
    gitignore:
    * Update gitgnore to ignore all test projects

--- Scripts~/sync_package_info.sh
+++ Scripts~/sync_package_info.sh
@@ -1,5 +1,18 @@
+allProjects=(
+    "MeshSync~"
+    "MeshSync_HDRP~"
+)
+
 packageName="com.unity.meshsync"
 
-cp package.json "MeshSync~\\Packages\\${packageName}"
-cp CHANGELOG.md "MeshSync~\\Packages\\${packageName}"    
+for project in ${allProjects[@]}; do    
+    cp package.json "${project}\\Packages\\${packageName}"
+    cp CHANGELOG.md "${project}\\Packages\\${packageName}"    
+done
+
+
+
+
+
+
 
