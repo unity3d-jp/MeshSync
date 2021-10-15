588f8d6c56d4e8b190184b544857c5e7b61c373d
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-10-15 12:29:57 +0900

    Add HDAdditionalLightDataExtensions

---
+++ Runtime/Scripts/Extensions/HDAdditionalLightDataExtensions.cs
@@ -0,0 +1,48 @@
+﻿
+#if AT_USE_HDRP
+
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+namespace Unity.MeshSync {
+
+internal static class HDAdditionalLightDataExtensions {
+
+    internal static void SetTypeFromLegacy(this HDAdditionalLightData lightData, LightType legacyLightType) {
+
+        switch (legacyLightType) {
+            case LightType.Directional: {
+                lightData.type = HDLightType.Directional;
+                break;
+            }
+            case LightType.Spot: {
+                lightData.type = HDLightType.Spot;
+                break;
+            }            
+            case LightType.Point: {
+                lightData.type = HDLightType.Point;
+                break;
+            }
+            case LightType.Disc: {
+                lightData.type           = HDLightType.Area;
+                lightData.areaLightShape = AreaLightShape.Disc;
+                break;
+            }
+            case LightType.Area: {
+                lightData.type           = HDLightType.Area;
+                lightData.areaLightShape = AreaLightShape.Rectangle;
+                break;
+            }
+            default: {
+                Debug.LogWarning($"[MeshSync] Unsupported legacy light: {legacyLightType}");
+                break;
+            };
+        
+        }        
+    }
+}
+
+} //end namespace
+
+#endif //#if AT_USE_HDRP    
+    
