62b733f0c7d5266809bf31293f7a3b1ac5498b83
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-10-18 22:24:19 +0900

    test

--- Tests/Editor/SceneCachePlayerTest.cs
+++ Tests/Editor/SceneCachePlayerTest.cs
@@ -96,12 +96,20 @@ public class SceneCachePlayerTest  {
         //Check the instanced prefab
         Assert.IsNotNull(player);              
         Assert.IsTrue(player.gameObject.IsPrefabInstance());              
-        Camera cam0   = player.GetComponentInChildren<Camera>();        
-        Light  light0 = player.GetComponentInChildren<Light>();
+        Camera       cam0   = player.GetComponentInChildren<Camera>();        
+        Light        light0 = player.GetComponentInChildren<Light>();
+        MeshRenderer mr0    = player.GetComponentInChildren<MeshRenderer>();
+        
+        Assert.IsNotNull(cam0);
+        Assert.IsNotNull(light0);
+        Assert.IsNotNull(mr0);
 
         GameObject capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
         capsule.transform.SetParent(light0.transform);
         
+        GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        cylinder.transform.SetParent(mr0.transform);
+                
         //Change
         TestDataComponents comps = ChangeSceneCacheFileAndVerify(player,Path.GetFullPath(MeshSyncTestEditorConstants.SPHERE_TEST_DATA_PATH));
         Assert.IsTrue(player.IsSceneCacheOpened());
@@ -110,6 +118,8 @@ public class SceneCachePlayerTest  {
         
         Assert.IsNotNull(capsule);
         Assert.IsFalse(capsule.IsPrefabInstance());
+        Assert.IsTrue(cylinder == null); //should have been deleted when changing sc
+        
 
         //Cleanup
         Object.DestroyImmediate(player.gameObject);
