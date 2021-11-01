a9df35ce63d17052a6b2954f9d0e82588c84beeb
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-11-01 12:10:12 +0900

    Add common importer settings

--- Runtime/Scripts/BaseMeshSync.cs
+++ Runtime/Scripts/BaseMeshSync.cs
@@ -2125,6 +2125,7 @@ public abstract class BaseMeshSync : MonoBehaviour, ISerializationCallbackReceiv
     [SerializeField] private Transform m_rootObject;
 
     [SerializeField] private protected MeshSyncPlayerConfig m_config;
+    [SerializeField] private protected CommonImporterSettings m_importerSettings;
     
     [SerializeField] private bool m_usePhysicalCameraParams = true;
     [SerializeField] private bool m_useCustomCameraMatrices = true;
