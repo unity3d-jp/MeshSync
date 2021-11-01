117ca15f468b7f10bc3278ebaa088d12c05803ba
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-11-01 12:04:58 +0900

    Add CommonImporterSettings

---
+++ Runtime/Scripts/Settings/Data/CommonImporterSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Unity.MeshSync {
+
+[Serializable]
+internal class CommonImporterSettings {
+
+    [SerializeField] private bool m_syncMaterials = true;    
+}
+
+} //end namespace
