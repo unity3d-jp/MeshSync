3a3a73f6ad269a46a61cafcc9efc0940b12662b6
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-11-01 23:57:11 +0900

    update

--- Editor/Scripts/SceneCache/SceneCacheImporterInspector.cs
+++ Editor/Scripts/SceneCache/SceneCacheImporterInspector.cs
@@ -8,7 +8,9 @@ public class SceneCacheImporterInspector: ScriptedImporterEditor
 {
     public override void OnInspectorGUI() {
 
+        serializedObject.Update();
         EditorGUILayout.PropertyField(serializedObject.FindProperty("m_importerSettings"));
+        serializedObject.ApplyModifiedProperties();
         base.ApplyRevertGUI();
     }
 
