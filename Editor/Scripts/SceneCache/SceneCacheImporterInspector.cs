32874b4a63badb9fe86d935d0fe952f984bbaaf4
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-11-01 16:58:46 +0900

    Draw ModelImporeterSettings

--- Editor/Scripts/SceneCache/SceneCacheImporterInspector.cs
+++ Editor/Scripts/SceneCache/SceneCacheImporterInspector.cs
@@ -1,4 +1,8 @@
 
+using Unity.FilmInternalUtilities.Editor;
+using UnityEditor.UIElements;
+using UnityEngine;
+
 namespace Unity.MeshSync.Editor {
 
 using UnityEditor;
@@ -7,9 +11,28 @@ using UnityEditor.Experimental.AssetImporters;
 [CustomEditor(typeof(SceneCacheImporter))]
 public class SceneCacheImporterInspector: ScriptedImporterEditor
 {
+    public override void OnEnable() {
+        base.OnEnable();
+        m_scImporter = target as SceneCacheImporter;
+
+    }
+    
     public override void OnInspectorGUI() {
+        DrawModelImporterSettingsGUI(m_scImporter, m_scImporter.GetModelImporterSettings());
         base.ApplyRevertGUI();
     }
+
+    static void DrawModelImporterSettingsGUI(Object obj, ModelImporterSettings settings) {
+        EditorGUIDrawerUtility.DrawUndoableGUI(obj, "SceneCache: Snap",            
+            guiFunc: () => (bool)EditorGUILayout.Toggle("Create Materials", settings.CreateMaterials), 
+            updateFunc: (bool createMat) => { settings.CreateMaterials = createMat; });
+        
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+    
+    private SceneCacheImporter m_scImporter = null;
+
 }
 
 
