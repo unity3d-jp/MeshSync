28272ac9568e43506bc3db1ced6f138acc127c05
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-11-01 23:56:22 +0900

    add test

---
+++ Tests/Editor/SceneCachePlayerImporterTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using UnityEditor;
+
+namespace Unity.MeshSync.Editor.Tests {
+
+
+public class SceneCachePlayerImporterTest  {
+
+//----------------------------------------------------------------------------------------------------------------------
+    [Test]
+    public void CheckImporterProperties() {
+        SceneCacheImporter importer         = new SceneCacheImporter();
+        SerializedObject   serializedObject = new SerializedObject(importer);
+        Assert.IsNotNull(serializedObject.FindProperty("m_importerSettings"));
+    }
+    
+}
+
+} //end namespace
