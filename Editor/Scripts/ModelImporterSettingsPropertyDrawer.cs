58b7bd873bf4a483cdfeb98d1fbd422b70aae71d
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-11-02 15:59:20 +0900

    use utility in property drawer

--- Editor/Scripts/ModelImporterSettingsPropertyDrawer.cs
+++ Editor/Scripts/ModelImporterSettingsPropertyDrawer.cs
@@ -7,14 +7,8 @@ namespace Unity.MeshSync.Editor {
 public class ModelImporterSettingsPropertyDrawer : PropertyDrawer {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
         
-        EditorGUI.BeginProperty(position, label, property);
-        
-        EditorGUILayout.PropertyField(property.FindPropertyRelative("CreateMaterials"), new GUIContent("Create Materials"));
-        
-        ++EditorGUI.indentLevel;        
-        EditorGUILayout.PropertyField(property.FindPropertyRelative("MaterialSearchMode"), new GUIContent("Material Search Mode"));
-        --EditorGUI.indentLevel;
-
+        EditorGUI.BeginProperty(position, label, property);        
+        MeshSyncInspectorUtility.DrawModelImporterSettingsGUI(property);
         EditorGUI.EndProperty();
     }    
 }
