35947fe5ee48150b06c49fcad7de93d6b4d24938
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-11-05 18:19:15 +0900

    use UIElementsEditorUtility

--- Editor/Scripts/ProjectSettings/ServerSettingsTab.cs
+++ Editor/Scripts/ProjectSettings/ServerSettingsTab.cs
@@ -33,7 +33,7 @@ internal class ServerSettingsTab : IMeshSyncSettingsTab {
         MeshSyncProjectSettings projectSettings = MeshSyncProjectSettings.GetOrCreateSettings();
         
         //Add server port
-        m_serverPortField = AddField<IntegerField,int>(fieldTemplate, content, Contents.ServerPort,
+        m_serverPortField = AddField<IntegerField,int>(content, Contents.ServerPort,
             projectSettings.GetDefaultServerPort(),
             (int newValue) => {
                 MeshSyncProjectSettings settings = MeshSyncProjectSettings.GetOrCreateSettings();
@@ -41,7 +41,7 @@ internal class ServerSettingsTab : IMeshSyncSettingsTab {
             }
         );
 
-        m_allowPublicAccessToggle = AddField<Toggle,bool>(fieldTemplate, content, Contents.AllowPublicAccess,
+        m_allowPublicAccessToggle = AddField<Toggle,bool>(content, Contents.AllowPublicAccess,
             projectSettings.GetServerPublicAccess(),
             (bool  newValue) => {
                 MeshSyncProjectSettings settings = MeshSyncProjectSettings.GetOrCreateSettings();
@@ -64,26 +64,16 @@ internal class ServerSettingsTab : IMeshSyncSettingsTab {
 //----------------------------------------------------------------------------------------------------------------------
     
     //Support Toggle, FloatField, etc
-    private F AddField<F,V>(VisualTreeAsset template, VisualElement parent, GUIContent content,
+    private F AddField<F,V>(VisualElement parent, GUIContent content,
         V initialValue, Action<V> onValueChanged) where F: VisualElement,INotifyValueChanged<V>, new()  
     {
-        TemplateContainer templateInstance = template.CloneTree();
-        VisualElement     fieldContainer   = templateInstance.Query<VisualElement>("FieldContainer").First();
-        Label             label            = templateInstance.Query<Label>().First();
-        label.text    = content.text;
-        label.tooltip = content.tooltip;
-        
-        F field = new F();
-        field.AddToClassList("project-settings-field");
-        field.SetValueWithoutNotify(initialValue);
-        field.RegisterValueChangedCallback((ChangeEvent<V> changeEvent) => {
-        
+        F field = UIElementsEditorUtility.AddField<F, V>(parent, content, initialValue, (ChangeEvent<V> changeEvent) => {
             onValueChanged(changeEvent.newValue);
             MeshSyncProjectSettings.GetOrCreateSettings().Save();
-        });        
-        
-        fieldContainer.Add(field);
-        parent.Add(templateInstance);
+        });
+
+        field.parent.AddToClassList("project-settings-field-container");
+        field.AddToClassList("project-settings-field");
         return field;
     }	
     
