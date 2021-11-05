6104ce97b5e2b9b76b6593760a03557c58abdb13
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-11-05 11:46:39 +0900

    Add ComponentSyncSettings

---
+++ Runtime/Scripts/Settings/Data/ComponentSyncSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Unity.MeshSync {
+
+[Serializable]
+internal class ComponentSyncSettings {
+    
+    [SerializeField] internal bool Create;
+    [SerializeField] internal bool Update;
+    
+}
+
+} //end namespace
