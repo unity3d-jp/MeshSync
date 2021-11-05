771915bc1d08a7751723732b2f01242a33d28e44
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-11-05 12:13:31 +0900

    add utility function

--- Editor/Scripts/Utilities/MeshSyncInspectorUtility.cs
+++ Editor/Scripts/Utilities/MeshSyncInspectorUtility.cs
@@ -45,6 +45,28 @@ internal static class MeshSyncInspectorUtility {
         }
     }
     
+//----------------------------------------------------------------------------------------------------------------------
+    
+    internal static void DrawComponentSyncSettings(Object obj, string prefixLabel,  ComponentSyncSettings syncSettings) {
+        EditorGUILayout.BeginHorizontal();
+        
+        EditorGUILayout.PrefixLabel(prefixLabel);        
+        EditorGUIDrawerUtility.DrawUndoableGUI(obj, "Sync: Create",            
+            guiFunc: () => (bool)GUILayout.Toggle(syncSettings.CanCreate,"Create"), 
+            updateFunc: (bool val) => { syncSettings.CanCreate = val; });
+
+        using (new EditorGUI.DisabledScope(!syncSettings.CanCreate)) {
+            
+            EditorGUIDrawerUtility.DrawUndoableGUI(obj, "Sync: Update",            
+                guiFunc: () => (bool)GUILayout.Toggle(syncSettings.CanUpdate,"Update"), 
+                updateFunc: (bool val) => { syncSettings.CanUpdate = val; });
+        }
+
+        EditorGUILayout.EndHorizontal();
+        
+    }
+    
+    
 }
 
 } //end namespace
