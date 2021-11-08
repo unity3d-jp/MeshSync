f123dc511aa7299ce5f16c306ad69a95c843d784
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-11-08 20:16:10 +0900

    add MeshSyncServerConfig

---
+++ Runtime/Scripts/Settings/Data/MeshSyncServerConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Unity.MeshSync {
+[Serializable]
+internal class MeshSyncServerConfig : MeshSyncPlayerConfig {
+
+    
+#pragma warning disable 414
+    [SerializeField] private int m_meshSyncServerConfigVersion = (int)MeshSyncPlayerConfigVersion.INITIAL;
+#pragma warning restore 414
+
+    private const int CUR_MESHSYNC_SERVER_CONFIG_VERSION = (int)MeshSyncPlayerConfigVersion.INITIAL; 
+
+    enum MeshSyncPlayerConfigVersion {
+        INITIAL = 1,
+    }
+   
+
+}
+} //end namespace
