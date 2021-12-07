1a803bcb3aac6eff60cfb9d3e7f9f3ce62ca1502
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-12-07 20:55:44 +0900

    add comment
    (cherry picked from commit 6c40a2f6e00d649c93e3a8151d6d4c16679d8701)

--- Editor/Scripts/BaseMeshSyncInspector.cs
+++ Editor/Scripts/BaseMeshSyncInspector.cs
@@ -155,6 +155,7 @@ internal abstract class BaseMeshSyncInspector : UnityEditor.Editor {
 //----------------------------------------------------------------------------------------------------------------------
     internal static bool DrawMaterialList(BaseMeshSync t, bool allowFold = true) {
 
+        //[TODO-sin: 2021-12-7] Refactor. Remove allowFold
         bool changed = false;
         if (allowFold) {
             var styleFold = EditorStyles.foldout;
