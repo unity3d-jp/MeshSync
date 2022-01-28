d7693b3a45d3adaa1d2864dfc9a8b637f0024a0d
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-01-28 16:57:20 +0900

    rebase

--- Runtime/Scripts/BitFlags.cs
+++ Runtime/Scripts/BitFlags.cs
@@ -1,27 +1,20 @@
 using System;
 using UnityEngine;
 
-namespace Unity.MeshSync
-{
-    [Serializable]
-    internal struct BitFlags
-    {
-        // int because uint is not serialized...
-        [SerializeField] public int bits;
+namespace Unity.MeshSync {
+[Serializable]
+internal struct BitFlags {
+    // int because uint is not serialized...
+    [SerializeField] public int bits;
 
-        public bool this[int v]
-        {
-            get
-            {
-                return (bits & (1 << v)) != 0;
-            }
-            set
-            {
-                if (value)
-                    bits |= (1 << v);
-                else
-                    bits &= ~(1 << v);
-            }
+    public bool this[int v] {
+        get { return (bits & (1 << v)) != 0; }
+        set {
+            if (value)
+                bits |= (1 << v);
+            else
+                bits &= ~(1 << v);
         }
     }
+}
 }
