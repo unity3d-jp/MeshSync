db40063768cab6603a1dcbaa880381dd6fcb6c0f
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-01 22:01:22 +0900

    rebase

--- Plugin~/Src/MeshSync/Include/MeshSync/SceneCache/msSceneCacheInputSettings.h
+++ Plugin~/Src/MeshSync/Include/MeshSync/SceneCache/msSceneCacheInputSettings.h
@@ -4,39 +4,12 @@
 #include "MeshSync/SceneCache/msSceneCacheEncoderSettings.h"
 
 #include "MeshSync/SceneCache/msSceneCacheInput.h"
-#include "MeshSync/SceneCache/msSceneCacheOutput.h"
 #include "MeshSync/SceneGraph/msSceneImportSettings.h"
 
 //Forward declarations
 msDeclClassPtr(SceneCacheInput)
-msDeclClassPtr(SceneCacheOutput)
 
 namespace ms {
-
-struct OSceneCacheSettingsBase
-{
-    // serialized in cache file
-    SceneCacheEncoding encoding = SceneCacheEncoding::ZSTD;
-    SceneCacheEncoderSettings encoder_settings;
-    float sample_rate = 30.0f; // 0.0f means 'variable sample rate'
-
-    // flags
-    uint32_t strip_unchanged : 1;
-    uint32_t apply_refinement : 1;
-    uint32_t flatten_hierarchy : 1;
-    uint32_t merge_meshes : 1; // todo
-    uint32_t strip_normals : 1;
-    uint32_t strip_tangents : 1;
-
-    OSceneCacheSettingsBase();
-};
-
-struct OSceneCacheSettings : OSceneCacheSettingsBase, SceneImportSettings
-{
-    // *not* serialized in cache file
-    int max_queue_size = 4;
-    int max_scene_segments = 8;
-};
 
 struct ISceneCacheSettingsBase
 {
@@ -53,9 +26,6 @@ struct ISceneCacheSettingsBase
 };
 struct ISceneCacheSettings : ISceneCacheSettingsBase, SceneImportSettings {};
 
-//[TODO-sin: 2022-2-28] Move these functions to the appropriate classes
-SceneCacheOutputPtr OpenOSceneCacheFile(const char *path, const OSceneCacheSettings& oscs = OSceneCacheSettings());
-SceneCacheOutput* OpenOSceneCacheFileRaw(const char *path, const OSceneCacheSettings& oscs = OSceneCacheSettings());
 SceneCacheInputPtr OpenISceneCacheFile(const char *path, const ISceneCacheSettings& iscs = ISceneCacheSettings());
 SceneCacheInput* OpenISceneCacheFileRaw(const char *path, const ISceneCacheSettings& iscs = ISceneCacheSettings());
 
