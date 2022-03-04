41a0601958c7cc103df414e12be16b69b25a9aa0
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-04 11:03:17 +0900

    comment
    [skip ci]

--- Plugin~/Src/MeshSync/Include/MeshSync/SceneCache/msSceneCacheOutputSettings.h
+++ Plugin~/Src/MeshSync/Include/MeshSync/SceneCache/msSceneCacheOutputSettings.h
@@ -6,10 +6,10 @@
 
 namespace ms {
 
+// NOT serialized in cache file
 struct SceneCacheOutputSettings : SceneImportSettings
 {
     SceneCacheExportSettings exportSettings;
-    // *not* serialized in cache file
     int max_queue_size = 4;
     int max_scene_segments = 8;
 };
