04f00f24044f06d93bafa1f0b3208ae638d20169
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-04 11:28:38 +0900

    const etc

--- Plugin~/Src/MeshSync/SceneCache/BufferEncoder.h
+++ Plugin~/Src/MeshSync/SceneCache/BufferEncoder.h
@@ -3,14 +3,13 @@
 #include "MeshSync/SceneGraph/msScene.h"
 
 msDeclClassPtr(BufferEncoder);
-msDeclClassPtr(Mesh);
 
 namespace ms {
 
 class BufferEncoder
 {
 public:
-    virtual ~BufferEncoder();
+    virtual ~BufferEncoder() = default;
     virtual void encode(RawVector<char>& dst, const RawVector<char>& src) = 0;
     virtual void decode(RawVector<char>& dst, const RawVector<char>& src) = 0;
 };
