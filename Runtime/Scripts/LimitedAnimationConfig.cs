18a1056947f23942533a2d0cdaa3b7f3bb185e7a
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-11 18:28:28 +0900

    apply

--- Runtime/Scripts/LimitedAnimationConfig.cs
+++ Runtime/Scripts/LimitedAnimationConfig.cs
@@ -15,6 +15,20 @@ internal class LimitedAnimationConfig {
     internal void SetFrameOffset(int offset ) { m_frameOffset = offset;}
     internal int  GetFrameOffset()            { return m_frameOffset;}
     
+//----------------------------------------------------------------------------------------------------------------------
+    
+    internal int Apply(int frame) {
+        if (!m_enabled) {
+            return frame;
+        }
+
+        m_numFramesToHold = Mathf.Max(1, m_numFramesToHold);
+        int multiplier = Mathf.FloorToInt((float) frame / m_numFramesToHold);
+        frame = multiplier * m_numFramesToHold;
+        return frame;
+    }
+    
+    
 //----------------------------------------------------------------------------------------------------------------------    
     [SerializeField] private bool m_enabled         = false;
     [SerializeField] private int  m_numFramesToHold = 1; //hold one data for several frames.
