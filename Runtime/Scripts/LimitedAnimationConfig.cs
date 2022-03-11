b46824f4b60dc698926f75b266ec1ad551d5e13a
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-11 18:19:11 +0900

    Add LimitedAnimationConfig

---
+++ Runtime/Scripts/LimitedAnimationConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Unity.MeshSync {
+
+[Serializable]
+internal class LimitedAnimationConfig {
+
+    internal void SetEnabled(bool enabled) { m_enabled = enabled;}
+    internal bool IsEnabled()              { return m_enabled;}
+
+    internal void SetNumFramesToHold(int numFrames) { m_numFramesToHold = numFrames;}
+    internal int  GetNumFramesToHold()              { return m_numFramesToHold;}
+
+    internal void SetFrameOffset(int offset ) { m_frameOffset = offset;}
+    internal int  GetFrameOffset()            { return m_frameOffset;}
+    
+//----------------------------------------------------------------------------------------------------------------------    
+    [SerializeField] private bool m_enabled         = false;
+    [SerializeField] private int  m_numFramesToHold = 1; //hold one data for several frames.
+    [SerializeField] private int  m_frameOffset = 0; 
+    
+}    
+    
+} //end namespace
