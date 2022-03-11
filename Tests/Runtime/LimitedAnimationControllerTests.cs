45dffb6369df152d668381aca2192418ca132d4b
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-11 18:55:54 +0900

    rename test

--- Tests/Runtime/LimitedAnimationControllerTests.cs
+++ Tests/Runtime/LimitedAnimationControllerTests.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 
 namespace Unity.MeshSync.Tests {
 
-internal class LimitedAnimationConfigTests {
+internal class LimitedAnimationControllerTests {
    
 //----------------------------------------------------------------------------------------------------------------------    
     [Test]
