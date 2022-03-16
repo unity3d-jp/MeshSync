5e268191a2c3a08f042947a9798155f4eef18f21
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-16 12:19:19 +0900

    refactor test

--- Tests/Editor/SceneCachePlayableAssetTests.cs
+++ Tests/Editor/SceneCachePlayableAssetTests.cs
@@ -20,27 +20,17 @@ internal class SceneCachePlayableAssetTests {
         EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects);
 
         //Add director
-        GameObject directorGo = new GameObject("Director");
-        PlayableDirector director = directorGo.AddComponent<PlayableDirector>();
+        PlayableDirector director = new GameObject("Director").AddComponent<PlayableDirector>();
         
-        //Setup scene cache            
-        GameObject       sceneCacheGo     = new GameObject();
-        SceneCachePlayer sceneCachePlayer = sceneCacheGo.AddComponent<SceneCachePlayer>();
-        Assert.IsFalse(sceneCachePlayer.IsSceneCacheOpened());       
-        SceneCachePlayerEditorUtility.ChangeSceneCacheFile(sceneCachePlayer, Path.GetFullPath(MeshSyncTestEditorConstants.CUBE_TEST_DATA_PATH));
-        
+        //Setup scene cache
+        SceneCachePlayer sceneCachePlayer = CreateTestSceneCachePlayer();
 
         //Create timeline asset
         TimelineAsset asset = ScriptableObject.CreateInstance<TimelineAsset>();
         director.playableAsset = asset;
 
         //Create PlayableAsset/Track/ etc
-        SceneCachePlayableAsset playableAsset   = ScriptableObject.CreateInstance<SceneCachePlayableAsset>();
-        SceneCacheTrack         sceneCacheTrack = asset.CreateTrack<SceneCacheTrack>(null, "TestSceneCacheTrack");
-        TimelineClip            clip       = sceneCacheTrack.CreateDefaultClip();
-        clip.asset = playableAsset;        
-        director.SetReferenceValue(playableAsset.GetSceneCachePlayerRef().exposedName, sceneCachePlayer );
-
+        TimelineClip clip = SceneCachePlayerEditorUtility.AddSceneCacheTrackAndClip(director, "TestSceneCacheTrack", sceneCachePlayer);
 
         //Selecting director in Timeline Window will trigger the TimelineWindow's update etc.
         TimelineEditorUtility.SelectDirectorInTimelineWindow(director);
@@ -50,22 +40,36 @@ internal class SceneCachePlayableAssetTests {
         yield return null;
         
         Assert.AreEqual(0, sceneCachePlayer.GetTime());
-        double timePerFrame = 1.0f / sceneCacheTrack.timelineAsset.editorSettings.GetFPS();
+        SceneCacheTrack sceneCacheTrack = clip.GetParentTrack() as SceneCacheTrack;
+        Assert.IsNotNull(sceneCacheTrack);
+        double          timePerFrame    = 1.0f / sceneCacheTrack.timelineAsset.editorSettings.GetFPS();
 
         double directorTime = clip.start + clip.duration - timePerFrame;
         SetDirectorTime(director, directorTime); //this will trigger change in the time of the SceneCachePlayable
         yield return null;
 
         //Check clipData and curve
+        SceneCachePlayableAsset playableAsset = clip.asset as SceneCachePlayableAsset;
+        Assert.IsNotNull(playableAsset);
         SceneCacheClipData clipData = playableAsset.GetBoundClipData();
         Assert.IsNotNull(clipData);        
-        AnimationCurve curve          = clipData.GetAnimationCurve();
+        AnimationCurve curve = clipData.GetAnimationCurve();
         Assert.IsNotNull(curve);
         float normalizedTime = curve.Evaluate((float)directorTime);
                 
         Assert.IsTrue(Mathf.Approximately((float)(normalizedTime * clip.duration), sceneCachePlayer.GetTime()));
     }
-   
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    private static SceneCachePlayer CreateTestSceneCachePlayer() {
+        GameObject       sceneCacheGo     = new GameObject();
+        SceneCachePlayer sceneCachePlayer = sceneCacheGo.AddComponent<SceneCachePlayer>();
+        SceneCachePlayerEditorUtility.ChangeSceneCacheFile(sceneCachePlayer, Path.GetFullPath(MeshSyncTestEditorConstants.CUBE_TEST_DATA_PATH));
+        return sceneCachePlayer;
+    }
+
     
 //----------------------------------------------------------------------------------------------------------------------
     private static void SetDirectorTime(PlayableDirector director, double time) {
