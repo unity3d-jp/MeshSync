f4d1f70dff29acb8eacc46d84965a46b82f4c00b
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-17 00:32:09 +0900

    Revert "fix: ensure the SceneCache gameobject is active when deleting"
    This reverts commit 306b695e33a30b6c384cca3e77f6f24a213acbbd.

--- Runtime/Scripts/SceneCache/SceneCachePlayableBehaviour.cs
+++ Runtime/Scripts/SceneCache/SceneCachePlayableBehaviour.cs
@@ -19,22 +19,14 @@ internal class SceneCachePlayableBehaviour : PlayableBehaviour {
     
 //----------------------------------------------------------------------------------------------------------------------        
     
-    public override void OnPlayableDestroy(Playable playable) {
-        //Check if OnBehaviourPause() was disabling the gameobject when the playable is deleted
-        if (!m_isGameObjectActive && m_lastActiveChangedTime == Time.frameCount) {
-            ActivateGameObject(true);
-        }
-    }
-    
     public override void OnBehaviourPlay(Playable playable, FrameData info) {
         ActivateGameObject(true);
     }
     
 
-    public override void OnBehaviourPause(Playable playable, FrameData info) {        
+    public override void OnBehaviourPause(Playable playable, FrameData info) {
         ActivateGameObject(false);
     }
-    
 
     public override void ProcessFrame(Playable playable, FrameData info, object playerData) {
         if (null == m_sceneCachePlayer) {
@@ -54,25 +46,13 @@ internal class SceneCachePlayableBehaviour : PlayableBehaviour {
     private void ActivateGameObject(bool active) {
         if (null == m_sceneCachePlayer)
             return;
-
-        GameObject go = m_sceneCachePlayer.gameObject;
-        if (go.activeSelf == active)
-            return;
-        
-        go.SetActive(active);
-        
-        m_isGameObjectActive    = go.activeSelf;
-        m_lastActiveChangedTime = Time.frameCount;
+        m_sceneCachePlayer.gameObject.SetActive(active);
     }
 //----------------------------------------------------------------------------------------------------------------------
-
-    private SceneCachePlayer m_sceneCachePlayer  = null;
+    
+    private SceneCachePlayer m_sceneCachePlayer = null;
     
     private SceneCacheClipData m_clipData = null;
-    
-    private int m_lastActiveChangedTime = 0;
-    private bool  m_isGameObjectActive = false;
-    
 }
 
 } //end namespace
