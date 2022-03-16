87bbacfac04e91628b21a23514a843cc2e682bb0
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-16 22:46:16 +0900

    merge fix

--- Tests/Editor/SceneCachePlayableAssetTests.cs
+++ Tests/Editor/SceneCachePlayableAssetTests.cs
@@ -84,6 +84,26 @@ internal class SceneCachePlayableAssetTests {
         yield return null;
         Assert.IsFalse(scGameObject.activeSelf);
     }
+
+//----------------------------------------------------------------------------------------------------------------------
+    public IEnumerator EnsureMatchingFramesAreLoadedToScene() {
+
+        InitTest(out PlayableDirector director, out SceneCachePlayer sceneCachePlayer, out TimelineClip clip);
+        yield return null;
+        SceneCacheData scData = sceneCachePlayer.GetSceneCacheData();
+        
+        int    numFrames    = scData.GetNumScenes();
+        double timePerFrame = 1.0f / scData.GetSampleRate();
+        
+        for(int i=0;i<numFrames-1;++i) {
+            
+            double directorTime = clip.start + i * timePerFrame;
+            SetDirectorTime(director, directorTime); //this will trigger change in the time of the SceneCachePlayable
+            yield return null;
+                       
+            Assert.AreEqual(i, sceneCachePlayer.GetFrame());
+        }
+    }
     
 //----------------------------------------------------------------------------------------------------------------------
 
