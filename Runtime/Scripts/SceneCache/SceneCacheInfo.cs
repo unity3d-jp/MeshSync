3aa9ee9096754a44f406d1b1806cc6c02e18af03
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-17 14:17:41 +0900

    Add SceneCacheInfo class

---
+++ Runtime/Scripts/SceneCache/SceneCacheInfo.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+namespace Unity.MeshSync {
+
+internal class SceneCacheInfo {
+    public  int            numFrames;
+    private TimeRange      timeRange;
+    public  float          sampleRate;
+    public  AnimationCurve timeCurve;
+}
+
+} //end namespace
