dbee371270a7d67fcc55bc6baaec5e6b6a74cead
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-17 14:54:32 +0900

    comment

--- Runtime/Scripts/SceneCache/SceneCacheInfo.cs
+++ Runtime/Scripts/SceneCache/SceneCacheInfo.cs
@@ -5,7 +5,7 @@ namespace Unity.MeshSync {
 
 internal class SceneCacheInfo {
     internal int            numFrames;
-    internal float          sampleRate;
+    internal float          sampleRate; // value <=0 indicates variable sample rate.
     internal TimeRange      timeRange;
     internal AnimationCurve timeCurve;
 }
