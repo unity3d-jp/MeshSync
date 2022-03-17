e2254221207e2df84351b0a5502ec70a18177536
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2022-03-17 20:28:24 +0900

    fix: clamp frameOffset after setting num frames to hold for limited animation (#598)
    * add test

--- Runtime/Scripts/LimitedAnimationController.cs
+++ Runtime/Scripts/LimitedAnimationController.cs
@@ -10,15 +10,18 @@ internal class LimitedAnimationController {
 
     internal LimitedAnimationController(bool enabled, int numFramesToHold, int frameOffset) {
         m_enabled         = enabled;
-        m_numFramesToHold = numFramesToHold;
-        m_frameOffset     = frameOffset;
+        SetNumFramesToHold(numFramesToHold);
+        SetFrameOffset(frameOffset);
     }    
     
 //----------------------------------------------------------------------------------------------------------------------
     internal void SetEnabled(bool enabled) { m_enabled = enabled;}
     internal bool IsEnabled()              { return m_enabled;}
 
-    internal void SetNumFramesToHold(int numFrames) { m_numFramesToHold = numFrames;}
+    internal void SetNumFramesToHold(int numFrames) {
+        m_numFramesToHold = numFrames;
+        SetFrameOffset(m_frameOffset);
+    }
     internal int  GetNumFramesToHold()              { return m_numFramesToHold;}
 
     internal void SetFrameOffset(int offset) {
