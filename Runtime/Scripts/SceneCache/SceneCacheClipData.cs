f9e7d66dd624d91ad9dd101ef40d74a4343c9483
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-18 09:58:15 +0900

    remove unnecessary code

--- Runtime/Scripts/SceneCache/SceneCacheClipData.cs
+++ Runtime/Scripts/SceneCache/SceneCacheClipData.cs
@@ -54,9 +54,6 @@ internal class SceneCacheClipData : BaseClipData {
 
         TimelineClip clip = GetOwner();
         Assert.IsNotNull(clip);
-        if (null == clip.curves) {
-            clip.CreateCurves("Curves: " + clip.displayName);
-        }
        
         //Bind for the first time
         m_scPlayer         = sceneCachePlayer;
