37ac9154fc1a8fd4a55de847365cbce4db7bf4ce
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-03-25 00:17:51 +0900

    importing old data

--- Runtime/Scripts/SceneCache/SceneCacheClipData.cs
+++ Runtime/Scripts/SceneCache/SceneCacheClipData.cs
@@ -30,6 +30,10 @@ internal class SceneCacheClipData : BaseClipData {
         if (m_sceneCacheClipDataVersion < (int) SceneCacheClipDataVersion.MovedLimitedAnimationController_0_12_6) {
             m_copyLimitedAnimationControllerToAsset = true;
         }
+
+        if (m_sceneCacheClipDataVersion < (int) SceneCacheClipDataVersion.MovedAnimationCurve_0_12_6) {
+            m_copyAnimationCurveToAsset = true;
+        }
         
         m_sceneCacheClipDataVersion = CUR_SCENE_CACHE_CLIP_DATA_VERSION;
     }
@@ -212,15 +216,20 @@ internal class SceneCacheClipData : BaseClipData {
 
     //[TODO-sin:2022-3-24] remove this in 0.13.x
     internal void CopyLegacyClipDataToAsset(SceneCachePlayableAsset sceneCachePlayableAsset) {
-        if (!m_copyLimitedAnimationControllerToAsset) {
-            return;
+        if (m_copyLimitedAnimationControllerToAsset) {
+            LimitedAnimationController assetController = sceneCachePlayableAsset.GetOverrideLimitedAnimationController();
+            assetController.SetEnabled(m_overrideLimitedAnimationController.IsEnabled());
+            assetController.SetFrameOffset(m_overrideLimitedAnimationController.GetFrameOffset());
+            assetController.SetNumFramesToHold(m_overrideLimitedAnimationController.GetNumFramesToHold());
+            m_copyLimitedAnimationControllerToAsset = false;
         }
-        
-        LimitedAnimationController assetController = sceneCachePlayableAsset.GetOverrideLimitedAnimationController();
-        assetController.SetEnabled(m_overrideLimitedAnimationController.IsEnabled());
-        assetController.SetFrameOffset(m_overrideLimitedAnimationController.GetFrameOffset());
-        assetController.SetNumFramesToHold(m_overrideLimitedAnimationController.GetNumFramesToHold());
-        m_copyLimitedAnimationControllerToAsset = false;
+
+
+        if (m_copyAnimationCurveToAsset) {
+            sceneCachePlayableAsset.SetAnimationCurve(m_animationCurve);
+            sceneCachePlayableAsset.SetSceneCacheCurveExtracted(m_initialized);
+            m_copyAnimationCurveToAsset = false;
+        }
     }
 //----------------------------------------------------------------------------------------------------------------------
    
@@ -241,12 +250,14 @@ internal class SceneCacheClipData : BaseClipData {
     private const int CUR_SCENE_CACHE_CLIP_DATA_VERSION = (int) SceneCacheClipDataVersion.MovedLimitedAnimationController_0_12_6;
 
     private bool m_copyLimitedAnimationControllerToAsset = false;
+    private bool m_copyAnimationCurveToAsset = false;
 
 //----------------------------------------------------------------------------------------------------------------------
 
     internal enum SceneCacheClipDataVersion {
         Initial = 1, 
         MovedLimitedAnimationController_0_12_6, //Moved LimitedAnimationController to SceneCachePlayableAsset in 0.12.6
+        MovedAnimationCurve_0_12_6, //Moved LimitedAnimationController to SceneCachePlayableAsset in 0.12.6
         
     } 
     
