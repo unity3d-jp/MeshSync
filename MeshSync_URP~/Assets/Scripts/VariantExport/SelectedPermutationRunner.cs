8300de9eb770cf6b7d34adc553011814f9bd4c72
Christian Schinkoethe <christian.schinkoethe@unity.com>
2022-03-31 16:38:30 +0100

    Adding new exporter

---
+++ MeshSync_URP~/Assets/Scripts/VariantExport/SelectedPermutationRunner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unity.MeshSync.VariantExport
+{
+    internal class SelectedPermutationRunner : PermutationRunnerBase
+    {
+        public override int PermutationCount
+        {
+            get
+            {
+                return variantExporter.Whitelist.Count;
+            }
+        }
+
+        public SelectedPermutationRunner(VariantExporter variantExporter) : base(variantExporter)
+        {
+        }
+
+        protected override IEnumerator ExecutePermutations(int propIdx)
+        {
+            foreach (var setting in variantExporter.Whitelist)
+            {
+                variantExporter.DeserializeProps(setting);
+
+                yield return new WaitForMeshSync(server);
+
+                yield return Save();
+
+                //yield break;
+            }
+
+            yield break;
+        }
+    }
+}
