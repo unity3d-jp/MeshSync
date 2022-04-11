d60de5b6f5eacb18882bcc96fd3b04eba90c2ae4
Sean Dillon <sean.dillon@unity3d.com>
2022-04-11 10:31:45 +0100

    Instance Rendering: formating issue fix

--- Plugin~/Src/MeshSync/Include/MeshSync/SceneGraph/msInstanceInfo.h
+++ Plugin~/Src/MeshSync/Include/MeshSync/SceneGraph/msInstanceInfo.h
@@ -5,45 +5,45 @@
 #include "MeshSync/SceneGraph/msMesh.h"
 
 namespace ms {
-	using namespace std;
-	using namespace mu;
-	class InstanceInfo
-	{
-		uint64_t m_hash = 0;
+using namespace std;
+using namespace mu;
+class InstanceInfo
+{
+	uint64_t m_hash = 0;
 
-	public:
+public:
 
-		/// <summary>
-		/// /// Path on the hierachy tree to the mesh that the instances refer to.
-		/// /// </summary>
-		string path;
+	/// <summary>
+	/// /// Path on the hierachy tree to the mesh that the instances refer to.
+	/// /// </summary>
+	string path;
 
-		string parent_path;
+	string parent_path;
 
-		/// <summary>
-		/// World transforms of the instances
-		/// </summary>
-		SharedVector<float4x4> transforms;
+	/// <summary>
+	/// World transforms of the instances
+	/// </summary>
+	SharedVector<float4x4> transforms;
 
-		void serialize(ostream& os);
-		void deserialize(istream& is);
+	void serialize(ostream& os);
+	void deserialize(istream& is);
 
-		InstanceInfo();
-		InstanceInfo(const InstanceInfo& v);
-		InstanceInfo& operator=(const InstanceInfo& v);
-		InstanceInfo(InstanceInfo&& v) noexcept; // noexcept to enforce std::vector to use move constructor
-		InstanceInfo& operator=(InstanceInfo&& v);
+	InstanceInfo();
+	InstanceInfo(const InstanceInfo& v);
+	InstanceInfo& operator=(const InstanceInfo& v);
+	InstanceInfo(InstanceInfo&& v) noexcept; // noexcept to enforce std::vector to use move constructor
+	InstanceInfo& operator=(InstanceInfo&& v);
 
-		msDefinePool(InstanceInfo);
+	msDefinePool(InstanceInfo);
 
-		void clear();
+	void clear();
 
-		static shared_ptr<InstanceInfo> create(std::istream& is);
-		uint64_t hash();
+	static shared_ptr<InstanceInfo> create(std::istream& is);
+	uint64_t hash();
 
-		Identifier getIdentifier();
+	Identifier getIdentifier();
 
-	};
-	msSerializable(InstanceInfo)
+};
+msSerializable(InstanceInfo)
 }
 
