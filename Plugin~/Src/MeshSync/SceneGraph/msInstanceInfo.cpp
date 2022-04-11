572e579836a68313215c778852afeb0e5b0a7d4b
Sean Dillon <sean.dillon@unity3d.com>
2022-04-11 10:31:45 +0100

    Instance Rendering: formating issue fix

--- Plugin~/Src/MeshSync/SceneGraph/msInstanceInfo.cpp
+++ Plugin~/Src/MeshSync/SceneGraph/msInstanceInfo.cpp
@@ -3,67 +3,67 @@
 #include <MeshSync/SceneGraph/msIdentifier.h>
 
 namespace ms {
-	void InstanceInfo::serialize(ostream& os)
-	{
-		write(os, path);
-		write(os, parent_path);
-		write(os, transforms);
+void InstanceInfo::serialize(ostream& os)
+{
+	write(os, path);
+	write(os, parent_path);
+	write(os, transforms);
 		
-	}
+}
 
-	void InstanceInfo::deserialize(istream& is)
-	{
-		read(is, path);
-		read(is, parent_path);
-		read(is, transforms);
-	}
-	void InstanceInfo::clear()
-	{
-		this->path = "";
-		this->parent_path = "";
-		this->transforms.clear();
-	}
+void InstanceInfo::deserialize(istream& is)
+{
+	read(is, path);
+	read(is, parent_path);
+	read(is, transforms);
+}
+void InstanceInfo::clear()
+{
+	this->path = "";
+	this->parent_path = "";
+	this->transforms.clear();
+}
 
-	shared_ptr<InstanceInfo> InstanceInfo::create(istream& is)
-	{
-		auto ret = InstanceInfo::create();
-		ret->deserialize(is);
-		return ret;
-	}
+shared_ptr<InstanceInfo> InstanceInfo::create(istream& is)
+{
+	auto ret = InstanceInfo::create();
+	ret->deserialize(is);
+	return ret;
+}
 
-	uint64_t InstanceInfo::hash()
-	{
-		return m_hash;
-	}
-	Identifier InstanceInfo::getIdentifier()
-	{
-		return Identifier{ path, InvalidID};
-	}
+uint64_t InstanceInfo::hash()
+{
+	return m_hash;
+}
+Identifier InstanceInfo::getIdentifier()
+{
+	return Identifier{ path, InvalidID};
+}
 
-	InstanceInfo::InstanceInfo() {}
+InstanceInfo::InstanceInfo() {}
 
-	InstanceInfo::InstanceInfo(const InstanceInfo& v)
-	{
-		*this = v;
-	}
+InstanceInfo::InstanceInfo(const InstanceInfo& v)
+{
+	*this = v;
+}
 
-	InstanceInfo& InstanceInfo::operator=(const InstanceInfo& v)
-	{
-		path = v.path;
-		transforms = v.transforms;
-		return *this;
-	}
+InstanceInfo& InstanceInfo::operator=(const InstanceInfo& v)
+{
+	path = v.path;
+	transforms = v.transforms;
+	return *this;
+}
 
-	InstanceInfo::InstanceInfo(InstanceInfo&& v) noexcept
-	{
-		*this = std::move(v);
-	}
+InstanceInfo::InstanceInfo(InstanceInfo&& v) noexcept
+{
+	*this = std::move(v);
+}
 
-	InstanceInfo& InstanceInfo::operator=(InstanceInfo&& v)
-	{
-		path = std::move(v.path);
-		transforms = std::move(v.transforms);
-		return *this;
-	}
+InstanceInfo& InstanceInfo::operator=(InstanceInfo&& v)
+{
+	path = std::move(v.path);
+	transforms = std::move(v.transforms);
+	return *this;
+}
 
 }
