fdd20c7c7779d7f9d5a80b31c28da1e40b32668e
Christian Schinkoethe <christian.schinkoethe@unity.com>
2022-06-03 14:18:06 +0100

    Adding script to create symlinks in the test projects. Preparing interface for unity UI methods.

---
+++ Runtime/Scripts/IDCCLauncher.cs
@@ -0,0 +1,23 @@
+﻿namespace Unity.MeshSync
+{
+    public enum RunMode
+    {
+        GUI,
+        Background,
+        Console
+    }
+
+    /// <summary>
+    /// Interface for classes that handle DCC tool control from inside Unity.
+    /// </summary>
+    public interface IDCCLauncher
+    {
+        RunMode runMode { get; set; }
+
+        void OpenDCCTool(UnityEngine.Object asset);
+
+        void DrawDCCToolVersion(BaseMeshSync player);
+
+        void Cleanup();
+    }
+}
