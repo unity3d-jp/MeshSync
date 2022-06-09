da7f045b45e233fc662f9342f894c752c4b40dcc
schinkowski <schinkoethe@gmail.com>
2022-06-09 02:36:49 +0100

    chore: prepare for bidirectional mesh support. (#690)
    * Adding script to create symlinks in the test projects.
    Preparing interface for unity UI methods.
    
    * Adding missing meta file
    
    * Removing file that fails tests
    
    * removing files that make tests fail
    
    * Making classes internal

---
+++ Editor/Scripts/DCCIntegration/Launchers/blender_interop.py
@@ -0,0 +1,12 @@
+﻿import bpy
+import time
+
+bpy.ops.meshsync.auto_sync()
+
+def run():
+    time.sleep(10)
+
+# Make sure blender doesn't shut down:
+if bpy.app.background:
+    while (True):
+        run()
