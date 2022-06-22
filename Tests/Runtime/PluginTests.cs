4b0bc241b00645736f8a1dab379891a110b6d8b3
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-06-22 18:29:56 +0900

    skip OSX

--- Tests/Runtime/PluginTests.cs
+++ Tests/Runtime/PluginTests.cs
@@ -1,14 +1,16 @@
 ﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace Unity.MeshSync.Tests {
 internal class PluginTests {
     
+    //Skip OSX because we don't have a OSXUniversal bundle for mscore 
     [Test]
+    [UnityPlatform(RuntimePlatform.WindowsPlayer, RuntimePlatform.LinuxPlayer)]
     public void CheckVersionValidity() {
-
         string version = Lib.GetPluginVersion();
         Assert.IsTrue(PackageVersion.TryParse(version, out _));
-
     }
     
 //----------------------------------------------------------------------------------------------------------------------
