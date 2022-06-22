145281ed7725f144bbd810f90cc5f45ba68615cf
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-06-22 10:41:52 +0900

    add plugin test

---
+++ Tests/Runtime/PluginTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+
+namespace Unity.MeshSync.Tests {
+internal class PluginTests {
+    
+    [Test]
+    public void CheckVersion() {
+
+        string version = Lib.GetPluginVersion();
+        Assert.IsFalse(string.IsNullOrEmpty(version));
+        
+    }
+    
+//----------------------------------------------------------------------------------------------------------------------
+    
+    
+}
+
+} //end namespace
