f5d39e9aa0a213a42bbc4c1edb697c11f04c3df1
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2022-06-22 20:18:54 +0900

    test: add a test to check version validity in runtime (#694)
    * skip OSX because there is no universal version of mscore.bundle

---
+++ Tests/Runtime/FIU/PackageLifeCycle.cs
@@ -0,0 +1,16 @@
+﻿namespace Unity.MeshSync.Tests {
+
+
+//[TODO-sin:2022-6-22] Move to FIU
+/// <summary>
+/// The lifecycle status of a package
+/// </summary>
+internal enum PackageLifecycle {
+    Invalid,
+    Experimental,
+    Preview,
+    Prerelease,
+    Released,
+}
+
+} //end namespace
