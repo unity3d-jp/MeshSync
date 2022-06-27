614acf423fcbb2f61a5a18d6fe7695ecb7b9dce8
Christian Schinkoethe <christian.schinkoethe@unity.com>
2022-06-27 16:13:32 +0100

    Reducing nesting

--- Editor/Scripts/DCCIntegration/Launchers/BlenderLauncher.cs
+++ Editor/Scripts/DCCIntegration/Launchers/BlenderLauncher.cs
@@ -214,19 +214,21 @@ namespace Unity.MeshSync.Editor
 
         public void Dispose()
         {
-            if (m_blenderProcess != null)
+            if (m_blenderProcess == null)
             {
-                try
-                {
-                    if (!m_blenderProcess.HasExited)
-                    {
-                        m_blenderProcess.Kill();
-                    }
-                }
-                catch { }
-
-                m_blenderProcess = null;
+                return;
             }
+
+            try
+            {
+                if (!m_blenderProcess.HasExited)
+                {
+                    m_blenderProcess.Kill();
+                }
+            }
+            catch { }
+
+            m_blenderProcess = null;
         }
 
         #endregion // IDCCLauncher
