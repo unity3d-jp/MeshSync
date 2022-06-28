34197a6eeaa5225176e1b21f15f3191d33502304
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-06-28 19:23:50 +0900

    test creating server gameobject
    [skip ci]

--- Tests/Runtime/PluginTests.cs
+++ Tests/Runtime/PluginTests.cs
@@ -3,12 +3,13 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-//Skip OSX because we don't have a OSXUniversal bundle for mscore 
+//Skip OSXPlayer because we don't have a OSXUniversal bundle for mscore 
 namespace Unity.MeshSync.Tests {
 internal class PluginTests {
     
     [Test]
-    [UnityPlatform(RuntimePlatform.WindowsPlayer, RuntimePlatform.LinuxPlayer)]
+    [UnityPlatform(RuntimePlatform.WindowsPlayer, RuntimePlatform.WindowsEditor, RuntimePlatform.LinuxPlayer, 
+        RuntimePlatform.LinuxEditor, RuntimePlatform.OSXEditor)]
     public void CheckVersionValidity() {
         string version = Lib.GetPluginVersion();
         Assert.IsTrue(PackageVersion.TryParse(version, out _));
@@ -17,8 +18,9 @@ internal class PluginTests {
 //----------------------------------------------------------------------------------------------------------------------
     
     [UnityTest]
-    [UnityPlatform(RuntimePlatform.WindowsPlayer, RuntimePlatform.LinuxPlayer)]
-    public IEnumerator StartTestServer() {
+    [UnityPlatform(RuntimePlatform.WindowsPlayer, RuntimePlatform.WindowsEditor, RuntimePlatform.LinuxPlayer, 
+        RuntimePlatform.LinuxEditor, RuntimePlatform.OSXEditor)]
+    public IEnumerator CreateServerGameObject() {
         MeshSyncServer goServer  = new GameObject("Server").AddComponent<MeshSyncServer>();
         goServer.SetAutoStartServer(true);
         yield return null;
