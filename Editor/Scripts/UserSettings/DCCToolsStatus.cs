7ed66a63d1e7a3c568efda3a01bd6ec831b1f68e
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2022-09-02 17:16:54 +0900

    Add DCCToolStatus

---
+++ Editor/Scripts/UserSettings/DCCToolsStatus.cs
@@ -0,0 +1,11 @@
+
+namespace Unity.MeshSync.Editor {
+
+internal enum DCCToolStatus {
+    DCC_VERSION_UNSUPPORTED       = 0,
+    INSTALLED_PLUGIN_INCOMPATIBLE = 10,
+    NEWER_PLUGIN_AVAILABLE        = 20,
+    READY                         = 30,
+}
+    
+} //end namespace
