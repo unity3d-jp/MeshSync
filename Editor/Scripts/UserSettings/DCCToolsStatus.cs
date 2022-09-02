179f7426b0e625f80ce44c5b8e66706691a4eb96
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2022-09-02 18:00:12 +0900

    modify status

--- Editor/Scripts/UserSettings/DCCToolsStatus.cs
+++ Editor/Scripts/UserSettings/DCCToolsStatus.cs
@@ -3,9 +3,10 @@ namespace Unity.MeshSync.Editor {
 
 internal enum DCCToolStatus {
     DCC_VERSION_UNSUPPORTED       = 0,
-    INSTALLED_PLUGIN_INCOMPATIBLE = 10,
-    NEWER_PLUGIN_AVAILABLE        = 20,
-    READY                         = 30,
+    NOT_INSTALLED                 = 10,
+    INSTALLED_PLUGIN_INCOMPATIBLE = 20,
+    NEWER_PLUGIN_AVAILABLE        = 30,
+    READY                         = 40,
 }
     
 } //end namespace
