8093eeaefe47043c4f28febc1473810ebb6e9a66
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-09-20 14:00:51 +0900

    uninstall

--- Editor/DCCInstallScripts/UninstallBlenderPlugin.py
+++ Editor/DCCInstallScripts/UninstallBlenderPlugin.py
@@ -2,3 +2,10 @@ import bpy
 
 bpy.ops.preferences.addon_remove(module='unity_mesh_sync')
 bpy.ops.wm.save_userpref()
+
+bpy.ops.preferences.addon_disable(module='MeshSyncClientBlender')
+bpy.ops.wm.save_userpref()
+
+bpy.ops.preferences.addon_remove(module='MeshSyncClientBlender')
+bpy.ops.wm.save_userpref()
+
