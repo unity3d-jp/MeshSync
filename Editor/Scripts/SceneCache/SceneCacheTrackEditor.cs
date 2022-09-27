9f3dfe777b52fe129643208737a5c8faa84498ab
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-09-27 18:35:10 +0900

    fix: disable applicable SceneCache gameobject when muting SceneCacheTrack

--- Editor/Scripts/SceneCache/SceneCacheTrackEditor.cs
+++ Editor/Scripts/SceneCache/SceneCacheTrackEditor.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using UnityEditor.Timeline;
-using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.Timeline;
 
 namespace Unity.MeshSync.Editor {
@@ -9,13 +9,19 @@ namespace Unity.MeshSync.Editor {
 internal class SceneCacheTrackEditor : TrackEditor {
     
     public override void OnTrackChanged(TrackAsset track) {
-
-        if (WasMuted(track)) {
-            Debug.Log("OnTrackChanged : " + track +  " muted " + track.muted);
-            
-        }
+        if (!WasMuted(track)) 
+            return;
         
+        //[Note-sin:2022-9-27] if the same SceneCache exists in another track in the same time, then this will deactivate and activate the object again.
+        //Ideally, we should detect if it's possible to avoid this, but this doesn't seem possible at the moment.
+        //Event order: Editor.OnTrackChanged() -> Mixer.PrepareFrame() -> Mixer.ProcessFrame()
+        SceneCacheTrack sceneCacheTrack = track as SceneCacheTrack;
+        Assert.IsNotNull(sceneCacheTrack);
+        if (sceneCacheTrack.IsAutoActivateObject())
+            DeactivateSceneCacheInTrack(sceneCacheTrack);
     }
+    
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------    
 
     //true if the track was muted, false otherwise
     bool WasMuted(TrackAsset track) {
@@ -32,7 +38,23 @@ internal class SceneCacheTrackEditor : TrackEditor {
         m_lastTrackMuted[track] = track.muted;
         return track.muted;
     }
+    
+    private static void DeactivateSceneCacheInTrack(SceneCacheTrack track) {
+        Assert.IsNotNull(track);
+        
+        foreach (TimelineClip clip in track.GetClips()) {
+            SceneCachePlayableAsset sceneCachePlayableAsset = clip.asset as SceneCachePlayableAsset;
+            Assert.IsNotNull(sceneCachePlayableAsset);
+            
+            SceneCachePlayer scPlayer = sceneCachePlayableAsset.GetSceneCachePlayer();
+            if (null == scPlayer)
+                continue;
 
+            scPlayer.gameObject.SetActive(false);
+        }
+    }
+    
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     private readonly Dictionary<TrackAsset, bool> m_lastTrackMuted = new Dictionary<TrackAsset, bool>();
 
