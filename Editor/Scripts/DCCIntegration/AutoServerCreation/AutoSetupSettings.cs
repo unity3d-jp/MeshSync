40ae26b371d15766bc84a6b3371cbeebf042dc2f
seandillon92 <sean.dillon@unity3d.com>
2022-09-27 11:11:53 +0100

    feat: Unity Editor server for executing editor commands (#769)
    * Auto-creation of server on scenes that do not have one. Added dialog for user to control if server should be created or not.
    
    * Show pop-up only once per session
    
    * Show the user prompt for creating a server only once after the install
    
    * Reverting change in MeshSyncConstants
    
    * Default behavior is to assume user has been prompted. Settings file is stored in Assets/MeshSyncAssets
    
    * Editor Command messages for adding a server to the scene via IPC
    
    * Editor Command messages for adding a server to the scene via IPC
    
    * Improved implementation by blocking until command is executed
    
    * Added editor command for returning path of project
    
    * Removed dead code
    
    * Removed unused argument from notifyEditorCommand call
    
    * 3 second timeout for server thread waiting the unity thread to reply to editor command message
    
    * 3 second timeout for server thread waiting the unity thread to reply to editor command message (dll recompilation)
    
    * Log error message when failed to add a server to the scene
    
    * Formatting fix
    
    * Remove accidently added cs file
    
    * Change scope of AutoSetup class to internal
    
    * AutoSetup settings file: changed extension to yml from txt
    
    * Bookeeping of incoming Editor command requests to avoid race conditions on reply
    
    * Added id utility for getting session and message ids
    
    * Fixed error with lifetime of reply string
    
    * Replaced Application.dataPath with copy of FilmInternalUtilities.Editor.AssetEditorUtility.GetApplicationRootPath
    
    * Replaced Application.dataPath with copy of FilmInternalUtilities.Editor.AssetEditorUtility.GetApplicationRootPath
    
    * More robust copy of strings in message reply buffer
    
    * Simplified string copy
    
    * recompiled mscore.dll

---
+++ Editor/Scripts/DCCIntegration/AutoServerCreation/AutoSetupSettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[FilePath("Assets/MeshSyncAssets/AutoSetupSettings.yml", FilePathAttribute.Location.ProjectFolder)]
+internal class AutoSetupSettings : ScriptableSingleton<AutoSetupSettings> {
+
+    [SerializeField] private ushort m_port = 8081;
+
+    internal ushort Port {
+        get {return m_port; }
+        set {
+            
+            if (m_port == value)
+                return;
+            
+            m_port = value;
+            Save(true);
+        }
+    }
+}
--- Plugin~/Src/MeshSync/msProtocol.cpp
+++ Plugin~/Src/MeshSync/msProtocol.cpp
@@ -191,6 +191,25 @@ void ServerLiveEditResponse::deserialize(std::istream& is)
     EachMember(msRead);
 }
 
+EditorCommandMessage::EditorCommandMessage() {}
+void EditorCommandMessage::serialize(std::ostream& os) const {
+    super::serialize(os);
+    write(os, command_type);
+}
+
+void EditorCommandMessage::deserialize(std::istream& is){
+    super::deserialize(is);
+    read(is, command_type);
+}
+
+void EditorCommandMessage::SetReply(const char* input) {
+    strncpy_s(reply, input, MAX_REPLY_SIZE);
+}
+
+const char* EditorCommandMessage::GetReply() {
+    return reply;
+}
+
 #undef EachMember
 
 } // namespace ms
