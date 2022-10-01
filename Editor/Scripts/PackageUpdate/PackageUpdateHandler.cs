6bb6e3257624739a51ce9c06b3ea354da490a2da
Sean Dillon <sean.dillon@unity3d.com>
2022-10-01 20:36:53 +0100

    added namespace to package update handler and changed public access to internal

--- Editor/Scripts/PackageUpdate/PackageUpdateHandler.cs
+++ Editor/Scripts/PackageUpdate/PackageUpdateHandler.cs
@@ -1,14 +1,14 @@
 using Unity.FilmInternalUtilities.Editor;
-using Unity.MeshSync;
-using Unity.MeshSync.Editor;
 using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.SceneManagement;
 using Object = UnityEngine.Object;
 
+
+namespace Unity.MeshSync.Editor{
+
 [InitializeOnLoad]
-public class PackageUpdateHandler
-{
+internal class PackageUpdateHandler {
     static PackageUpdateHandler() {
         Events.registeredPackages -= OnPackageRegistered;
         Events.registeredPackages += OnPackageRegistered;
@@ -20,18 +20,18 @@ public class PackageUpdateHandler
     private static void OnPackageRegistering(PackageRegistrationEventArgs obj) {
         if (obj.changedFrom.FindPackage("com.unity.meshsync") == null)
             return;
-        
+
         StopAllServers();
     }
-    
+
     private static void OnPackageRegistered(PackageRegistrationEventArgs obj) {
-        
+
         if (obj.changedTo.FindPackage("com.unity.meshsync") == null)
             return;
-        
+
         // Save the current open scenes
         EditorSceneManager.SaveOpenScenes();
-        
+
         // If the package was just updated, stop all servers and restart the editor
         StopAllServers();
 
@@ -40,15 +40,16 @@ public class PackageUpdateHandler
     }
 
     private static void StopAllServers() {
-        
+
         // Stop editor server
         EditorServer.StopServer();
-        
+
         // Stop scene servers
         var servers = Object.FindObjectsOfType<MeshSyncServer>();
         foreach (var server in servers) {
-            
+
             server.StopServer();
         }
     }
+}
 }
