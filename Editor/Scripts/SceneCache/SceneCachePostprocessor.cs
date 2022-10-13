015de0c5e95ef99e48bbe839a6101c95ce39b4bf
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-10-13 23:18:16 +0900

    unify extension

--- Editor/Scripts/SceneCache/SceneCachePostprocessor.cs
+++ Editor/Scripts/SceneCache/SceneCachePostprocessor.cs
@@ -5,14 +5,17 @@ namespace Unity.MeshSync.Editor {
 
 class SceneCachePostprocessor : AssetPostprocessor {
     static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) {
+        
         foreach (string movedAssetPath in movedAssets) {
             string ext = Path.GetExtension(movedAssetPath);
-            if (ext != ".sc")
+            if (ext != DOT_SCENE_CACHE_EXTENSION)
                 continue;
             
             AssetDatabase.ImportAsset(movedAssetPath);
         }
     }
+
+    private static readonly string DOT_SCENE_CACHE_EXTENSION = $".{MeshSyncEditorConstants.SCENE_CACHE_EXTENSION}";
 }
 
 
