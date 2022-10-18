85bf91c1896e0ee633e86374b51287678d882594
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-10-18 18:28:27 +0900

    Draw OverLay

--- Editor/Scripts/SceneCache/FrameMarker/FrameMarkerEditor.cs
+++ Editor/Scripts/SceneCache/FrameMarker/FrameMarkerEditor.cs
@@ -7,6 +7,29 @@ namespace Unity.MeshSync.Editor {
 class FrameMarkerEditor : MarkerEditor {
 
     public override void DrawOverlay(IMarker m, MarkerUIStates uiState, MarkerOverlayRegion region) {
+        FrameMarker marker = m as FrameMarker;
+        if (null == marker)
+            return;
+
+        SISPlayableFrame playableFrame = marker.GetOwner();
+        
+        //Check invalid PlayableFrame/ClipData. Perhaps because of unsupported Duplicate operation ?
+        PlayableFrameClipData clipData = playableFrame?.GetOwner();
+        if (clipData == null)
+            return;
+        
+        KeyFrameMode keyFrameMode = (KeyFrameMode) playableFrame.GetProperty(KeyFramePropertyID.Mode);
+        
+        switch (keyFrameMode) {
+            case KeyFrameMode.Smooth: {
+                UnityEngine.Graphics.DrawTexture(region.markerRegion, EditorTextures.GetKeyFrameSmoothTexture());
+                break;
+            }
+            case KeyFrameMode.Stop: {
+                UnityEngine.Graphics.DrawTexture(region.markerRegion, EditorTextures.GetKeyFrameStopTexture());
+                break;
+            }
+        }
         
     }
 }
