13efcca9ede3a484da66a2cf1d96f7df7f7e3fb5
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-10-18 22:13:25 +0900

    reduce code in EditorTextures

--- Editor/Scripts/EditorTextures.cs
+++ Editor/Scripts/EditorTextures.cs
@@ -7,33 +7,27 @@ internal static class EditorTextures {
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     internal static Texture GetKeyFrameSmoothTexture() {
-        if (!m_keyFrameSmoothTexture.IsNullRef()) {
-            return m_keyFrameSmoothTexture;
-        }
-
-
-        m_keyFrameSmoothTexture = (Texture2D)EditorGUIUtility.Load("d_PlayButton On@2x");
-        return m_keyFrameSmoothTexture;
+        return GetOrLoadTexture(ref m_keyFrameSmoothTexture, "d_PlayButton On@2x");
     }
 
     internal static Texture GetKeyFrameStopTexture() {
-        if (!m_keyFrameStopTexture.IsNullRef()) {
-            return m_keyFrameStopTexture;
-        }
-
-        m_keyFrameStopTexture = (Texture2D)EditorGUIUtility.Load("d_PauseButton@2x");
-        return m_keyFrameStopTexture;
+        return GetOrLoadTexture(ref m_keyFrameStopTexture, "d_PauseButton@2x");
+        
     }
 
     internal static Texture GetKeyFrameBackgroundTexture() {
-        if (!m_keyFrameBackGroundTexture.IsNullRef()) {
-            return m_keyFrameBackGroundTexture;
-        }
-
-        m_keyFrameBackGroundTexture = (Texture2D)EditorGUIUtility.Load("gameviewbackground");
-        return m_keyFrameBackGroundTexture;
+        return GetOrLoadTexture(ref m_keyFrameBackGroundTexture, "gameviewbackground");
     }
 
+    static Texture GetOrLoadTexture(ref Texture tex, string texPath) {
+        if (!tex.IsNullRef()) {
+            return tex;
+        }
+
+        tex = (Texture2D)EditorGUIUtility.Load(texPath);
+        return tex;
+    }
+    
 //----------------------------------------------------------------------------------------------------------------------
 
     private static Texture m_keyFrameSmoothTexture     = null;
