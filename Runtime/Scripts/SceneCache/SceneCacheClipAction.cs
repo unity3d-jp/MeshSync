09e647df9d3a6ad2e96b4ffb4923f53708c63851
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-10-18 20:10:16 +0900

    add SceneCacheClipAction

---
+++ Runtime/Scripts/SceneCache/SceneCacheClipAction.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine.Timeline;
+using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
+
+namespace Unity.MeshSync {
+
+[MenuEntry("Add KeyFrame", MenuPriority.MarkerActionSection.start)]
+//[Shortcut(Shortcuts.Clip.trimStart), UsedImplicitly]
+class AddKeyFrameSceneCacheClipAction : ClipAction {
+    public override ActionValidity Validate(IEnumerable<TimelineClip> clips) {
+
+        if (clips.Contains<SceneCachePlayableAsset>()) {
+            return ActionValidity.Valid;
+        }
+        
+        return ActionValidity.NotApplicable;
+    }
+
+    public override bool Execute(IEnumerable<TimelineClip> clips) {
+        return true;
+    }
+}
+
+[MenuEntry("Delete KeyFrame", MenuPriority.MarkerActionSection.start)]
+//[Shortcut(Shortcuts.Clip.trimStart), UsedImplicitly]
+class DeleteKeyFrameSceneCacheClipAction : ClipAction {
+    public override ActionValidity Validate(IEnumerable<TimelineClip> clips) {
+        if (clips.Contains<SceneCachePlayableAsset>()) {
+            return ActionValidity.Valid;
+        }
+        return ActionValidity.NotApplicable;
+    }
+
+    public override bool Execute(IEnumerable<TimelineClip> clips) {
+        return true;
+    }
+}
+
+
+} //end namespace
