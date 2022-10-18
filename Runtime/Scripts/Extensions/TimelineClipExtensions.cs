77c46239696206ec7b1a6715e205b32a6ae89c51
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-10-18 20:10:05 +0900

    Add TimelineClipExtensions

---
+++ Runtime/Scripts/Extensions/TimelineClipExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace Unity.MeshSync {
+internal static class TimelineClipExtensions {
+    internal static bool Contains<T>(this IEnumerable<TimelineClip> clips) where T : PlayableAsset {
+        foreach (TimelineClip clip in clips) {
+            T sceneCachePlayableAsset = clip.asset as T;
+            if (null != sceneCachePlayableAsset)
+                return true;
+        }
+        return false;
+    }
+}
+} //end namespace
