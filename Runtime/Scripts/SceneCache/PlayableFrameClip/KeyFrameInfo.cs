8539aca6b5025ce66e288114bbc2ecec326c3419
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-10-19 16:32:13 +0900

    Add missing KeyFrameInfo

---
+++ Runtime/Scripts/SceneCache/PlayableFrameClip/KeyFrameInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Unity.MeshSync {
+
+[Serializable]
+internal struct KeyFrameInfo {
+    [SerializeField] internal bool         enabled;
+    [SerializeField] internal int          frameNo;
+    [SerializeField] internal KeyFrameMode mode;
+}
+
+} //end namespace
