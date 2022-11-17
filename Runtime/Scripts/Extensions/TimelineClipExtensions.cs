858054aede213d0189c8bd9719756f15431fc152
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-11-17 21:42:53 +0900

    rename variable

--- Runtime/Scripts/Extensions/TimelineClipExtensions.cs
+++ Runtime/Scripts/Extensions/TimelineClipExtensions.cs
@@ -4,10 +4,12 @@ using UnityEngine.Timeline;
 
 namespace Unity.MeshSync {
 internal static class TimelineClipExtensions {
+    
+    //[TODO-sin: 2022-11-17] Move to FIU
     internal static bool Contains<T>(this IEnumerable<TimelineClip> clips) where T : PlayableAsset {
         foreach (TimelineClip clip in clips) {
-            T sceneCachePlayableAsset = clip.asset as T;
-            if (null != sceneCachePlayableAsset)
+            T asset = clip.asset as T;
+            if (null != asset)
                 return true;
         }
         return false;
