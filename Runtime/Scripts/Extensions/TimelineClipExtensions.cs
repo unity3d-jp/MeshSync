a1030140c8b57cf75d08b29dc773780b27a3b74e
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2022-11-18 14:32:16 +0900

    feat: initial classes to support key frames in SceneCacheTrack (#828)
    * set version defines when using timeline
    * prepare shortcuts and add shortcut constants
    * bring related classes from StreamingImageSequence and modify them accordingly
    * add StyleSheets and KeyFrameMarker pictures
    * add KeyFrameMode
    * Add KeyFrameInfo
    * set keyframeMode using KeyFrameMode enum
    * add EditorTextures
    * add TimelineClipExtensions
    * add SaveStateFromMarker() function
    * add a function to check if we need to refresh KeyFrames in KeyFrameControllerClipData
    * allow stepping curve
    * refresh Timeline when editing frame
    * change the shortcut to change keyframe mode

---
+++ Runtime/Scripts/Extensions/TimelineClipExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace Unity.MeshSync {
+internal static class TimelineClipExtensions {
+    
+    //[TODO-sin: 2022-11-17] Move to FIU
+    internal static bool Contains<T>(this IEnumerable<TimelineClip> clips) where T : PlayableAsset {
+        foreach (TimelineClip clip in clips) {
+            T asset = clip.asset as T;
+            if (null != asset)
+                return true;
+        }
+        return false;
+    }
+}
+} //end namespace
