8154273bfd40594fabbcd1157bd1077b1aa5eb67
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-11-21 15:02:28 +0900

    Remove obsolete code

--- Runtime/Scripts/SceneCache/SceneCacheClipData.cs
+++ Runtime/Scripts/SceneCache/SceneCacheClipData.cs
@@ -32,26 +32,6 @@ internal class SceneCacheClipData : BaseClipData {
 
     internal override void DestroyV() { }
 
-
-//----------------------------------------------------------------------------------------------------------------------
-
-    //[TODO-sin:2022-3-24] remove this in 0.13.x
-    internal void CopyLegacyClipDataToAsset(SceneCachePlayableAsset sceneCachePlayableAsset) {
-        if (m_copyLimitedAnimationControllerToAsset) {
-            LimitedAnimationController assetController = sceneCachePlayableAsset.GetOverrideLimitedAnimationController();
-            assetController.SetEnabled(m_overrideLimitedAnimationController.IsEnabled());
-            assetController.SetFrameOffset(m_overrideLimitedAnimationController.GetFrameOffset());
-            assetController.SetNumFramesToHold(m_overrideLimitedAnimationController.GetNumFramesToHold());
-            m_copyLimitedAnimationControllerToAsset = false;
-        }
-
-
-        if (m_copyAnimationCurveToAsset) {
-            sceneCachePlayableAsset.SetAnimationCurve(m_animationCurve);
-            sceneCachePlayableAsset.SetIsSceneCacheCurveExtracted(m_initialized);
-            m_copyAnimationCurveToAsset = false;
-        }
-    }
 //----------------------------------------------------------------------------------------------------------------------
    
     [SerializeField] private AnimationCurve m_animationCurve = AnimationCurve.Constant(0,0,0);
