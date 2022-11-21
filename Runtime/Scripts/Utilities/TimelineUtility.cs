aaf55c7acae43891abf9844133763874b6ebd48b
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-11-21 13:01:12 +0900

    internal

--- Runtime/Scripts/Utilities/TimelineUtility.cs
+++ Runtime/Scripts/Utilities/TimelineUtility.cs
@@ -5,7 +5,7 @@ namespace Unity.MeshSync {
 
 internal static class TimelineUtility {
 
-    static void DeleteInvalidMarkers(TrackAsset track) {
+    internal static void DeleteInvalidMarkers(TrackAsset track) {
         List<KeyFrameMarker> markersToDelete = new List<KeyFrameMarker>();
         foreach(IMarker m in track.GetMarkers()) {
             KeyFrameMarker marker = m as KeyFrameMarker;
