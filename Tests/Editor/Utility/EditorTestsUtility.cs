d8d2ac66a4c83fa4784818375a595ab02b33aa3a
Sindharta Tanuwijaya <sindharta@gmail.com>
2023-01-24 17:19:17 +0900

    add EditorTestsUtility

---
+++ Tests/Editor/Utility/EditorTestsUtility.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace Unity.MeshSync.Editor.Tests {
+internal class EditorTestsUtility {
+    
+    internal static PlayableDirector CreateTestDirector() {
+        PlayableDirector director = new GameObject("Director").AddComponent<PlayableDirector>();
+        TimelineAsset    asset    = ScriptableObject.CreateInstance<TimelineAsset>();
+        director.playableAsset = asset;
+        return director;
+    }
+}
+} //end namespace
