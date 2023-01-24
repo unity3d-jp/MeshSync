f69c84c31906a1487ef36fd9c1d589ff91f62fb3
Sindharta Tanuwijaya <sindharta@gmail.com>
2023-01-24 17:28:20 +0900

    add InitTest

--- Tests/Editor/Utility/EditorTestsUtility.cs
+++ Tests/Editor/Utility/EditorTestsUtility.cs
@@ -1,10 +1,27 @@
 ﻿using System.IO;
+using Unity.FilmInternalUtilities.Editor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
 
 namespace Unity.MeshSync.Editor.Tests {
 internal static class EditorTestsUtility {
+
+    private static void InitTest(bool enableSceneCacheGo, out PlayableDirector director, out SceneCachePlayer sceneCachePlayer, out TimelineClip clip) {
+        EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects);
+
+        director         = CreateDirector();
+        sceneCachePlayer = CreateSceneCachePlayer();
+        sceneCachePlayer.gameObject.SetActive(enableSceneCacheGo);
+        clip = SceneCachePlayerEditorUtility.AddSceneCacheTrackAndClip(director, "TestSceneCacheTrack", sceneCachePlayer);
+
+        TimelineEditorUtility.SelectDirectorInTimelineWindow(director); //trigger the TimelineWindow's update etc.
+        TimelineEditorUtility.RefreshTimelineEditor();
+    }
+    
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
     internal static PlayableDirector CreateDirector() {
         PlayableDirector director = new GameObject("Director").AddComponent<PlayableDirector>();
         TimelineAsset    asset    = ScriptableObject.CreateInstance<TimelineAsset>();
@@ -12,7 +29,6 @@ internal static class EditorTestsUtility {
         return director;
     }
 
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------
     
     internal static SceneCachePlayer CreateSceneCachePlayer() {
         GameObject       sceneCacheGo     = new GameObject();
