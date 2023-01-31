0818f81f13d817614f51b522025268946cb4c04f
seandillon92 <sean.dillon@unity3d.com>
2023-01-31 01:22:50 +0000

    fix: parallax slider set to min value when using BiRP or URP (#893)
    The code that applies the material data properties is checking if the keyword is used in the material and skips it if not. Since BiRP and URP are using the "_Parallax" keyword and HDRP does not, there is no need to check the rendering pipeline.
    
    Details:
    * Extracted interface and created custom material data class. Used custom AO strength material data to set to 0.25
    * HDRP fix
    * change parallax values to 0.005

--- Runtime/Scripts/CustomMaterialPropertyData.cs
+++ Runtime/Scripts/CustomMaterialPropertyData.cs
@@ -4,6 +4,15 @@ namespace Unity.MeshSync
 {
     internal class CustomMaterialPropertyData : IMaterialPropertyData
     {
+        public CustomMaterialPropertyData()
+        {
+        }
+
+        public CustomMaterialPropertyData(IMaterialPropertyData data)
+        {
+            Copy(data);
+        }
+
         public string name { get; set; }
         public int nameID { get; set; }
         public IMaterialPropertyData.Type type { get; set; }
@@ -16,5 +25,21 @@ namespace Unity.MeshSync
         public float[] floatArray { get; set; }
         public Vector4[] vectorArray { get; set; }
         public Matrix4x4[] matrixArray { get; set; }
+
+        public void Copy(IMaterialPropertyData data)
+        {
+            this.name = data.name;
+            this.nameID = data.nameID;
+            this.type = data.type;
+            this.intValue = data.intValue;
+            this.floatValue = data.floatValue;
+            this.vectorValue = data.vectorValue;
+            this.matrixValue = data.matrixValue;
+            this.textureValue = data.textureValue;
+            this.arrayLength = data.arrayLength;
+            this.floatArray = data.floatArray;
+            this.vectorArray = data.vectorArray;
+            this.matrixArray = data.matrixArray;
+        }
     }
 }
